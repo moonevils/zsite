768506d57a29e96f9ad953748be2d1d48635ec23
guanxiying <daitingting@xirangit.com>
2015-12-16 13:50:32 +0800

    * Add upgrade to 5.0.1 function.

--- system/module/upgrade/model.php
+++ system/module/upgrade/model.php
@@ -132,6 +132,7 @@ class upgradeModel extends model
                 $this->execSQL($this->getUpgradeFile('4.5.2'));
             case '4_6':
                 $this->execSQL($this->getUpgradeFile('4.6'));
+            case '5_0';
             default: if(!$this->isError()) $this->loadModel('setting')->updateVersion($this->config->version);
         }
 
@@ -185,6 +186,7 @@ class upgradeModel extends model
             case '4_5_1'    : $confirmContent .= file_get_contents($this->getUpgradeFile('4.5.1'));
             case '4_5_2'    : $confirmContent .= file_get_contents($this->getUpgradeFile('4.5.2'));
             case '4_6'      : $confirmContent .= file_get_contents($this->getUpgradeFile('4.6'));
+            case '5_0';
         }
         return str_replace(array('xr_', 'eps_'), $this->config->db->prefix, $confirmContent);
     }
