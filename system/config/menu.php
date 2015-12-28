a0dc045b42e5d85b85109b9d96cb73b30fa93c19
guanxiying <daitingting@xirangit.com>
2015-12-28 13:58:52 +0800

    * Fix default link of group in different modules.

--- system/config/menu.php
+++ system/config/menu.php
@@ -1,7 +1,7 @@
 <?php
 $config->menus = new stdclass();
 $config->menus->home   = 'admin,order,message,comment,reply,thread,forumreply';
-$config->menus->content = 'article,blog,book,page';
+$config->menus->content = 'article,blog,book,page,contribution';
 $config->menus->shop    = 'order,product,express,orderSetting';
 $config->menus->user    = 'user,message,reply,forum,wechat';
 $config->menus->promote = 'stat,tag,links,setstat,';
