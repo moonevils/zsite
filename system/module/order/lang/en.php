6e64ae3f327270d88866fdf28ac3ef39f8d91f41
guanxiying <daitingting@xirangit.com>
2015-12-29 08:52:19 +0800

    * Finish task 1778.

--- system/module/order/lang/en.php
+++ system/module/order/lang/en.php
@@ -22,7 +22,7 @@ $lang->order->expressInfo    = 'Express Information';
 $lang->order->receiver       = 'Receiver';
 $lang->order->noRecord       = 'No record';
 $lang->order->status         = 'Status';
-$lang->order->note           = 'Note';
+$lang->order->note           = 'Message to seller';
 
 $lang->order->admin          = 'Admin Orders';
 $lang->order->setting        = 'Settings';
