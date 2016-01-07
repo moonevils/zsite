a16b5fd7a1899fa4d6721b58031c749b0dd30460
guanxiying <daitingting@xirangit.com>
2016-01-07 14:30:29 +0800

    * Add default admin rights.

---
+++ system/config/admin.php
@@ -0,0 +1,11 @@
+<?php
+/* For guest users. */
+$config->rights->admin['admin']['index']         = 'index';
+$config->rights->admin['admin']['ignore']        = 'ignore';
+$config->rights->admin['admin']['ignoreupgrade'] = 'ignoreupgrade';
+
+$config->rights->admin['misc']['ping'] = 'ping';
+
+$config->rights->admin['visual']['editpowerby'] = 'editpowerby';
+
+$config->rights->admin['guarder']['getemailcode'] = 'getemailcode';
