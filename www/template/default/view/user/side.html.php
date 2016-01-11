ff427dd1b0f2e30fecd9fe2c919b35ee136dd906
Catouse <catouse@gmail.com>
2016-01-11 10:14:22 +0800

    * change UI of user control nav.

--- www/template/default/view/user/side.html.php
+++ www/template/default/view/user/side.html.php
@@ -1,10 +1,18 @@
+<style>
+.user-control-nav li.nav-parent {border: 1px solid #ddd;}
+.user-control-nav > li > span {background-color:#f5f5f5; border:none; color: #808080; padding: 8px 15px; display: block;}
+.user-control-nav > li.nav-heading {font-weight:bold;}
+.user-control-nav li > a > .nav-icon  {display: inline-block; width: 30px}
+.user-control-nav.nav-primary.nav-stacked > li > a {border: none}
+</style>
+
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <div class='col-md-2'>
   <ul class='nav nav-primary nav-stacked user-control-nav'>
     <li class='nav-heading'><?php echo $lang->user->control->common;?></li>
     <?php foreach($this->config->user->navGroups as $group => $items):?>
     <li class='nav-parent'>
-    <?php echo html::a('###', $lang->user->navGroups->$group);?>
+    <span><?php echo $lang->user->navGroups->$group;?></span>
     <ul class='nav'>
     <?php $navs = explode(',', $items);?>
     <?php foreach($navs as $nav)
@@ -15,7 +23,7 @@
         if(!commonModel::isAvailable($module)) continue;
         if($module == $this->app->getModuleName() && $method == $this->app->getMethodName()) $class .= 'active';
 
-        echo '<li class="' . $class . '">' . html::a($this->createLink($module, $method), $label) . '</li>';
+        echo '<li class="nav-icon ' . $class . '">' . html::a($this->createLink($module, $method), $label) . '</li>';
     }
     ?>
   </ul>
@@ -23,9 +31,4 @@
     <?php endforeach;?>
   </ul>
 </div>
-<style>
-.nav-parent ul.nav{padding:0 20px;border:}
-.nav-primary.nav-stacked > li > a{background-color:#F8F8F8; border:none;}
-li.nav-parent {border: 1px solid #ddd;}
-.nav-primary.nav-stacked > li.nav-heading  {font-weight:bold;}
-</style>
+
