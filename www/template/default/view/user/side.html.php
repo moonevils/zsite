d7dcbc6168b19474ad0bffc664b8085e029d13c9
guanxiying <daitingting@xirangit.com>
2016-02-17 13:21:14 +0800

    * Adjust menu style of user control.

--- www/template/default/view/user/side.html.php
+++ www/template/default/view/user/side.html.php
@@ -1,19 +1,14 @@
 <style>
-.user-control-nav li.nav-parent {border: 1px solid #ddd;}
-.user-control-nav > li > span {background-color:#f5f5f5; border:none; color: #808080; padding: 8px 15px; display: block;}
-.user-control-nav > li.nav-heading {font-weight:bold;}
-.user-control-nav li > a > .nav-icon  {display: inline-block; width: 30px}
-.user-control-nav.nav-primary.nav-stacked > li > a {border: none}
+ul.user-control-nav > li > a{ padding:8px 18px;}
+ul.user-control-nav > li.nav-icon > a{border-top:none; border-bottom:dashed 1px #ddd;}
+ul.user-control-nav > li.nav-icon.active > a{border-bottom:none}
+ul.user-control-nav > li.nav-icon:last-child > a{ border-bottom:1px solid #DDD;}
 </style>
-
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <div class='col-md-2'>
   <ul class='nav nav-primary nav-stacked user-control-nav'>
-    <li class='nav-heading'><?php echo $lang->user->control->common;?></li>
     <?php foreach($this->config->user->navGroups as $group => $items):?>
-    <li class='nav-parent'>
-    <span><?php echo $lang->user->navGroups->$group;?></span>
-    <ul class='nav'>
+    <li class='nav-heading'> <?php echo $lang->user->navGroups->$group;?></li>
     <?php $navs = explode(',', $items);?>
     <?php foreach($navs as $nav)
     {
@@ -23,13 +18,9 @@
         list($label, $module, $method) = explode('|', $menu);
         if(!commonModel::isAvailable($module)) continue;
         if($module == $this->app->getModuleName() && $method == $this->app->getMethodName()) $class .= 'active';
-
         echo '<li class="nav-icon ' . $class . '">' . html::a($this->createLink($module, $method), $label) . '</li>';
     }
     ?>
-  </ul>
-    </li>
     <?php endforeach;?>
   </ul>
 </div>
-
