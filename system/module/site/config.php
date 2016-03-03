20fd7f03a3dc2ee8511654c13c5588967908c09e
guanxiying <daitingting@xirangit.com>
2016-03-03 14:55:45 +0800

    * Adjust custom css path.

--- system/module/site/config.php
+++ system/module/site/config.php
@@ -11,8 +11,8 @@ if(!isset($config->site->mobileTemplate)) $config->site->mobileTemplate = 'close
 
 global $app;
 $config->site->ui = new stdclass();
-if($config->multi)  $config->site->ui->customCssFile = $app->getDataRoot() . 'css/' . $config->site->code . '/%s/%s.css';
-if(!$config->multi) $config->site->ui->customCssFile = $app->getDataRoot() . 'css/%s/%s.css';
+if($config->multi)  $config->site->ui->customCssFile = $app->getDataRoot() . 'css/' . $config->site->code . '/%s_%s.css';
+if(!$config->multi) $config->site->ui->customCssFile = $app->getDataRoot() . 'css/%s_%s.css';
 
 $config->themeSetting = new stdclass();
 $config->themeSetting->primaryColor = '#D1270A' ;
--- www/template/default/common/header.lite.html.php
+++ www/template/default/common/header.lite.html.php
@@ -68,15 +68,15 @@ $theme          = $this->config->template->{$this->device}->theme ? $this->confi
   }
 
   /* Import customed css file if it exists. */
-  $siteCustomCssFile = $this->app->getDataRoot() . 'css' . DS . $config->site->code . DS . $this->config->template->{$this->device}->name . DS . $this->config->template->{$this->device}->theme . '.css';
+  $siteCustomCssFile = $this->app->getDataRoot() . 'css' . DS . $config->site->code . DS . $this->config->template->{$this->device}->name . '_' . $this->config->template->{$this->device}->theme . '.css';
   if($config->multi && file_exists($siteCustomCssFile))
   {
-      css::import(sprintf($webRoot . 'data/css/%s/%s/%s.css?' . $this->config->template->customVersion, $config->site->code, $config->template->{$this->device}->name, $config->template->{$this->device}->theme), "id='themeStyle' data-template='{$template}' data-theme='{$theme}' data-device='{$this->device}'");
+      css::import(sprintf($webRoot . 'data/css/%s/%s_%s.css?' . $this->config->template->customVersion, $config->site->code, $config->template->{$this->device}->name, $config->template->{$this->device}->theme), "id='themeStyle' data-template='{$template}' data-theme='{$theme}' data-device='{$this->device}'");
   }
   else
   {
-      $customCssFile = $this->app->getDataRoot() . 'css' . DS . $this->config->template->{$this->device}->name . DS . $this->config->template->{$this->device}->theme . '.css';
-      if(file_exists($customCssFile)) css::import(sprintf($webRoot . 'data/css/%s/%s.css?' . $this->config->template->customVersion, $config->template->{$this->device}->name, $config->template->{$this->device}->theme), "id='themeStyle' data-template='{$template}' data-theme='{$theme}' data-device='{$this->device}'");
+      $customCssFile = $this->app->getDataRoot() . 'css' . DS . $this->config->template->{$this->device}->name . '_' . $this->config->template->{$this->device}->theme . '.css';
+      if(file_exists($customCssFile)) css::import(sprintf($webRoot . 'data/css/%s_%s.css?' . $this->config->template->customVersion, $config->template->{$this->device}->name, $config->template->{$this->device}->theme), "id='themeStyle' data-template='{$template}' data-theme='{$theme}' data-device='{$this->device}'");
   }
 
   if(isset($pageCSS)) css::internal($pageCSS);
