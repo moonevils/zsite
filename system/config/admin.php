9ccdc400d63ed72198cc2ebbc7b5bd4d03510f4e
daitingting <daitingting@xirangit.com>
2016-03-07 16:19:13 +0800

    * Add rights for admin.

--- system/config/admin.php
+++ system/config/admin.php
@@ -2,7 +2,13 @@
 /* For guest users. */
 $config->rights->admin['admin']['index']         = 'index';
 
-$config->rights->admin['misc']['ping'] = 'ping';
+$config->rights->admin['misc']['ping']   = 'ping';
+$config->rights->admin['misc']['about']  = 'about';
+$config->rights->admin['misc']['thanks'] = 'thanks';
+
+$config->rights->admin['user']['changepassword'] = 'changepassword';
+$config->rights->admin['user']['editemail']      = 'editemail';
+$config->rights->admin['user']['securityquestion'] = 'securityquestion';
 
 $config->rights->admin['visual']['editpowerby'] = 'editpowerby';
 
