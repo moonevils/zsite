3747064773b8dfbb09d5c2a143d15c27c590731e
guanxiying <daitingting@xirangit.com>
2016-03-10 16:13:21 +0800

    * adjust set logo pagge.

--- system/module/ui/view/setlogo.html.php
+++ system/module/ui/view/setlogo.html.php
@@ -15,50 +15,41 @@
   <div class='panel-heading'>
     <strong><i class='icon-certificate'></i><?php echo $lang->ui->setLogo;?></strong>
   </div>
-  <div class='panel-body'>
+  <div class='panel-body row row-logo'>
     <form method='post' id='ajaxForm' enctype='multipart/form-data'>
-      <table class='table table-form table-'>
-        <tr>
-          <th class='w-100px' rowspan='2'><?php echo $lang->ui->logo . $lang->colon;?></th>
-          <td>
-            <?php if(isset($logo->webPath)) echo html::image($logo->webPath, "class='logo'");?>
-            <?php if(!isset($logo->webPath)) echo "<div class='alert alert-info w-200px text-center'>{$lang->ui->noLogo}</div>";?>
-          </td>
-        </tr>
-        <tr>
-          <td><?php echo html::file('logo', "class='form-control'");?></td>
-          <td><?php echo html::select('theme', $lang->ui->logoList, '', "class='form-control'");?></td>
-          <td>
-            <strong class='text-info'>
-              <?php if($this->device == 'desktop') printf($lang->ui->suitableLogoSize, '50px-80px', '80px-240px');?>
-              <?php if($this->device == 'mobile') printf($lang->ui->suitableLogoSize, '<50px', '50px-200px');?>
-            </strong>
-          </td>
-        </tr>
-        <tr>
-          <td></td>
-        </tr>
-        <tr>
-          <th class='w-100px' rowspan='2'><?php echo $lang->ui->favicon;?></th>
-          <td class=''>
-            <?php if(isset($favicon->webPath)) echo html::image($favicon->webPath, "class='favicon'");?>
-            <?php if(!isset($favicon->webPath)) echo "<div class='alert alert-danger w-120px text-center'>{$lang->ui->noFavicon}</div>";?>
-          </td>
-        </tr>
-        <tr>
-          <td><?php echo html::file('favicon', "class='form-control'");?></td>
-        </tr>
-        <tfoot>
-        <tr>
-          <th></th>
-          <td class=''>
-            <?php echo html::submitButton();?>
-            <?php if($favicon or $defaultFavicon) commonModel::printLink('ui', 'deleteFavicon', '', $lang->ui->deleteFavicon, "class='btn'");?>
-            <?php if(isset($logo->webPath)) commonModel::printLink('ui', 'deleteLogo', '', $lang->ui->deleteLogo, "class='btn'");?>
-          </td>
-        </tr>
-        </tfoot>
-      </table>
+      <div class='col-md-6'>
+        <div class='box'>
+        <div class='card'>
+        <?php if(isset($logo->webPath)) echo html::a('javascript:;', html::image($logo->webPath, "class='logo'"), "class='btn-upload'");?>
+        <span class='actions'>
+        <?php if(isset($logo->webPath)) echo html::a('javascript:;', "<i class='icon icon-lg icon-edit-sign'> </i>", "class='text-info btn-editor'");?>
+        <?php if(isset($logo->webPath)) commonModel::printLink('ui', 'deleteLogo', '', "<i class='icon icon-lg icon-remove-sign'> </i>", "class='text-danger btn-deleter'");?>
+        </span>
+        <?php if(!isset($logo->webPath)) echo html::a('javascript:;', $lang->ui->uploadLogo, "class='text-lg btn-upload'");?>
+        </div>
+        <div class='text-important'>
+          <?php if($this->device == 'desktop') printf($lang->ui->suitableLogoSize, '50px-80px', '80px-240px');?>
+          <?php if($this->device == 'mobile') printf($lang->ui->suitableLogoSize, '<50px', '50px-200px');?>
+          <div class='hide'><?php echo html::file('logo', "class='form-control'");?></div>
+        </div>
+        </div>
+      </div>
+      <div class='col-md-6'>
+        <div class='box'>
+        <div class='card'>
+          <?php if(isset($favicon->webPath)) echo html::a('javascript:;', html::image($favicon->webPath, "class='favicon'"), "class='btn-upload'");?>
+          <span class='actions'>
+          <?php if(isset($logo->webPath)) echo html::a('javascript:;', "<i class='icon icon-lg icon-edit-sign'> </i>", "class='text-info btn-editor'");?>
+          <?php if($favicon or $defaultFavicon) commonModel::printLink('ui', 'deleteFavicon', '', "<i class='icon icon-lg icon-remove-sign'> </i>", "class='text-danger btn-deleter'");?>
+          </span>
+          <?php if(!isset($favicon->webPath)) echo html::a('javascript:;', $lang->ui->uploadFavicon, "class='text-lg btn-upload'");?>
+        </div>
+        <div class='text-important'>
+        <?php printf($lang->ui->faviconHelp, "http://api.chanzhi.org/goto.php?item=help_favicon");?>
+        </div>
+        <div class='hide'><?php echo html::file('favicon', "class='form-control'");?></div>
+        </div>
+      </div>
     </form>
   </div>
 </div>
