c8afc7836a92e33289a5ee817c1d8eb1929f02d7
guanxiying <daitingting@xirangit.com>
2016-03-11 15:00:19 +0800

    * Add searchbar file.

---
+++ www/template/default/block/searchbar.html.php
@@ -0,0 +1,14 @@
+<?php if(commonModel::isAvailable('search')): ?>
+  <div id='searchbar' data-ve='search'>
+    <form action='<?php echo helper::createLink('search')?>' method='get' role='search'>
+      <div class='input-group'>
+        <?php $keywords = ($this->app->getModuleName() == 'search') ? $this->session->serachIngWord : '';?>
+        <?php echo html::input('words', $keywords, "class='form-control' placeholder=''");?>
+        <?php if($this->config->requestType == 'GET') echo html::hidden($this->config->moduleVar, 'search') . html::hidden($this->config->methodVar, 'index');?>
+        <div class='input-group-btn'>
+          <button class='btn btn-default' type='submit'><i class='icon icon-search'></i></button>
+        </div>
+      </div>
+    </form>
+  </div>
+<?php endif;?>
