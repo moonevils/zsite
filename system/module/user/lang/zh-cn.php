301f9db8a30fd4d0cccae7a6dc3e4c7eafdac801
guanxiying <daitingting@xirangit.com>
2016-03-11 19:08:04 +0800

    * Check admin runmode.

--- system/module/user/lang/zh-cn.php
+++ system/module/user/lang/zh-cn.php
@@ -217,15 +217,17 @@ $lang->user->control->welcome     = '欢迎您，<strong>%s</strong>';
 $lang->user->control->lblPassword = "留空，则保持不变。";
 
 $lang->user->control->menus[10] = '<i class="icon-user"></i> 个人信息 <i class="icon-chevron-right"></i>|user|profile';
-if(RUN_MODE != 'install'))
+if(RUN_MODE != 'install')
 {
     if(commonModel::isAvailable('message')) $lang->user->control->menus[20] = '<i class="icon-comments-alt"></i> 我的消息 <i class="icon-chevron-right"></i>|user|message';
     if(commonModel::isAvailable('contribution')) $lang->user->control->menus[21] = '<i class="icon-envelope"></i> 我的投稿 <i class="icon-chevron-right"></i>|article|contribution'; 
+
     if(commonModel::isAvailable('score'))
     {
         $lang->user->control->menus[30] = '<i class="icon-sun"></i> 积分详情 <i class="icon-chevron-right"></i>|user|score';
         if(strpos($this->config->shop->payment, 'alipay') !== false) $lang->user->control->menus[40] = '<i class="icon-bolt"></i> 积分充值 <i class="icon-chevron-right"></i>|score|buyscore';
     }
+
     if(commonModel::isAvailable('forum'))
     {
         $lang->user->control->menus[50] = '<i class="icon-comment"></i> 我的主题 <i class="icon-chevron-right"></i>|user|thread';
@@ -234,7 +236,10 @@ if(RUN_MODE != 'install'))
 
     if(commonModel::isAvailable('order')) $lang->user->control->menus[25] = '<i class="icon-shopping-cart"></i> 我的订单 <i class="icon-chevron-right"></i>|order|browse';
     if(commonModel::isAvailable('shop')) $lang->user->control->menus[26] = '<i class="icon-map-marker"> </i> 地址管理 <i class="icon-chevron-right"></i>|address|browse';
-    if(commonModel::isAvailable('shop') or commonModel::isAvailable('score')) $lang->user->navGroups->order   = '订单信息';
+}
+if(RUN_MODE == 'admin')
+{
+    if(RUN_MODE == 'admin' and commonModel::isAvailable('shop') or commonModel::isAvailable('score')) $lang->user->navGroups->order   = '订单信息';
     if(commonModel::isAvailable('forum')) $lang->user->navGroups->message = '主题消息';
 }
 $lang->user->log = new stdclass();
