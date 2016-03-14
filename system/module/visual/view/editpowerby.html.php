9d76b3b79901bba42395ddce0c31bce7f6567c74
guanxiying <daitingting@xirangit.com>
2016-03-14 16:12:13 +0800

    * Adjust editpowerby info.

--- system/module/visual/view/editpowerby.html.php
+++ system/module/visual/view/editpowerby.html.php
@@ -1,6 +1,5 @@
 <?php include 'header.html.php'; ?>
-
 <div class='panel-body'>
-  <?php echo $lang->visual->editpowerbycontent;?>
+  <?php echo $lang->visual->editPowerBy;?>
 </div>
 <?php include 'footer.html.php'; ?>
