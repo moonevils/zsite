acca8a9d48a2069cc87555d9d84822cdf5cbae97
guanxiying <daitingting@xirangit.com>
2016-04-18 19:23:31 +0800

    * use widget.

--- system/module/admin/view/index.html.php
+++ system/module/admin/view/index.html.php
@@ -40,111 +40,45 @@
     </div>
   </div>
   <?php endif;?>
-
+  <div class='text-right panel-control'>
+    <a href='javascript:;' class='refresh-all-panel'><i class='icon-repeat'></i></a>
+    <a data-toggle='modal' href='<?php echo $this->createLink("widget", "create"); ?>' title='<?php echo $lang->widget->create; ?>'><i class='icon-plus'></i></a>
+  </div>
   <div class='row summary'>
-    <?php if(commonModel::isAvailable('order')):?>
-    <div class='col-xs-6'>
-      <div class='panel'>
-        <div class='panel-heading'><strong><?php echo $lang->admin->order;?></strong></div>
-        <div class='panel-body'>
-          <table class='table table-hover table-condensed table-borderless'>
-          <?php foreach($orders as $order):?> 
-          <?php $orderTitle = sprintf($lang->admin->orderTitle, $order->account, $currencySymbol . $order->amount);?>
-            <tr>
-              <td><?php commonModel::printLink('order', 'admin','', $orderTitle);?></td>
-              <td><?php echo substr($order->createdDate, -8);?></td>
-            </tr>
-          <?php endforeach;?>
-          <?php if(count($orders) == 5):?>
-            <tr>
-              <td></td><td align='right'><?php commonModel::printLink('order', 'admin', '', $lang->more . '...');?></td>
-            </tr>
-          <?php endif;?>
-          </table>
+    <?php
+    $index = 0;
+    reset($widgets);
+    ?>
+    <?php foreach($widgets as $key => $widget):?>
+    <?php
+    $index = $key;
+    list($moreModule, $moreMethod, $moreParams) = explode('|', $widget->moreLink);
+    ?>
+    <div class='col-xs-<?php echo $widget->grid;?> pull-left'>
+      <div class='panel <?php if(isset($widget->params->color)) echo 'panel-' . $widget->params->color;?>' id='widget<?php echo $index?>' data-id='<?php echo $index?>' data-name='<?php echo $widget->title?>' data-url='<?php echo $this->createLink('entry', 'printBlock', 'index=' . $index) ?>'>
+        <div class='panel-heading'>
+          <div class='panel-actions'>
+            <?php if(isset($widget->moreLink)) echo html::a(helper::createLink($moreModule, $moreMethod, $moreParams), $lang->more . "<i class='icon-double-angle-right'></i>", "class='more app-btn'");?>
+            <button class="btn btn-mini refresh-panel" type='button'><i class="icon-repeat"></i></button>
+            <div class='dropdown'>
+              <button role="button" class="btn btn-mini" data-toggle="dropdown" type='button'><span class="caret"></span></button>
+              <ul class="dropdown-menu pull-right" role="menu">
+                <li><a href="<?php echo $this->createLink("widget", "edit", "index=$widget->id"); ?>" data-toggle='modal' class='edit-widget' data-title='<?php echo $widget->title; ?>' data-icon='icon-pencil'><i class="icon-pencil"></i> <?php echo $lang->edit;?></a></li>
+                <li><a href="javascript:;" class="remove-panel"><i class="icon-remove"></i> <?php echo $lang->delete; ?></a></li>
+                <?php if($widget->type == 'html'):?>
+                  <li><a href="javascript:hiddenBlock(<?php echo $index;?>)" class="hidden-panel"><i class='icon-eye-close'></i> <?php echo $lang->index->hidden; ?></a></li>
+                <?php endif;?>
+              </ul>
+            </div>
+          </div>
+          <?php echo $widget->title;?>
+        </div>
+        <div class='panel-body no-padding'>
+          <?php echo $this->fetch('widget', 'printWidget', 'widget=' . $widget->id);?>
         </div>
       </div>
     </div>
-    <?php endif;?>
-    <?php if(commonModel::isAvailable('forum')):?>
-    <div class='col-xs-6'>
-      <div class='panel'>
-        <div class='panel-heading'><strong><?php echo $lang->admin->thread;?></strong></div>
-        <div class='panel-body'>
-          <table class='table table-hover table-condensed table-borderless'>
-          <?php foreach($threads as $thread):?> 
-          <?php $threadTitle = $thread->author . '&nbsp&nbsp' . helper::substr($thread->title, 25);?>
-            <tr>
-              <td><?php echo html::a(commonmodel::createFrontLink('thread', 'view', "threadid=$thread->id"), $threadTitle);?></td>
-              <td><?php echo substr($thread->addedDate, -8);?></td>
-            </tr>
-          <?php endforeach;?>
-          <?php if(count($threads) == 5):?>
-            <tr>
-              <td></td><td align='right'><?php commonModel::printLink('forum', 'admin', "tab=feedback", $lang->more . '...');?></td>
-            </tr>
-          <?php endif;?>
-          </table>
-        </div>
-      </div>
-    </div>
-    <?php endif;?>
-    <div class='col-xs-6'>
-      <div class='panel'>
-        <div class='panel-heading'><strong><?php echo $lang->admin->feedback;?></strong></div>
-        <div class='panel-body'>
-          <table class='table table-hover table-condensed table-borderless'>
-          <?php foreach($messages as $type => $message):?> 
-          <?php if($message != '0'):?>
-          <?php $messageTitle = sprintf($lang->admin->$type, $message);?>
-          <tr>
-            <td><?php commonModel::printLink('message', 'admin', "type={$type}", $messageTitle);?></td>
-          </tr>
-          <?php endif;?>
-          <?php endforeach;?>
-          <?php if(!empty($threadReply) and $threadReply != '0'):?>
-          <tr>
-            <?php $threadReplyTitle = sprintf($lang->admin->threadReply, $threadReply);?>
-            <td><?php commonModel::printLink('reply', 'admin', "order=id_desc&tab=feedback", $threadReplyTitle);?></td>
-          </tr>
-          <?php endif;?>
-          <?php if(commonModel::isAvailable('submittion') and $submittionCount != '0'):?>
-          <?php $submittionTitle = sprintf($lang->admin->submittion, $submittionCount);?>
-          <tr>
-            <td><?php commonModel::printLink('article', 'admin','type=submittion&tab=feedback', $submittionTitle);?></td>
-          </tr>
-          <?php endif;?>
-          <?php if(!empty($todayReportTitle)):?>
-          <tr>
-            <?php $todayReportTitle = sprintf($lang->admin->todayReport, $todayReport->pv, $todayReport->uv, $todayReport->ip);?>
-            <td><?php commonModel::printLink('stat', 'traffic', "mode=today", $todayReportTitle);?></td>
-          </tr>
-          <?php endif;?>
-          <?php if(!empty($yestodayReportTitle)):?>
-          <tr>
-            <?php $yestodayReportTitle = sprintf($lang->admin->yestodayReport, $yestodayReport->pv, $yestodayReport->uv, $yestodayReport->ip);?>
-            <td><?php commonModel::printLink('stat', 'traffic', "mode=yestoday", $yestodayReportTitle);?></td>
-          </tr>
-          <?php endif;?>
-          </table>
-        </div>
-      </div>
-    </div>
-    <div class='col-xs-6'>
-      <div class='row panel'>
-        <div class='panel-heading'><strong><?php echo $lang->admin->shortcuts->common;?></strong></div>
-        <div class='panel-body shortcutGroup'>
-          <?php 
-            if(!empty($articleCategories)) echo html::a($this->createLink('article', 'create'), $lang->admin->shortcuts->article, "class='btn btn-default'");
-            else echo html::a($this->createLink('tree', 'browse',"type=article"), $lang->admin->shortcuts->articleCategories, "class='btn btn-default'")
-          ?>
-          <?php echo html::a($this->createLink('product', 'create'), $lang->admin->shortcuts->product, "class='btn btn-default'");?>
-          <?php echo html::a($this->createLink('message', 'admin'), $lang->admin->shortcuts->feedback, "class='btn btn-default'");?>  
-          <?php echo html::a($this->createLink('site', 'setBasic'), $lang->admin->shortcuts->site, "class='btn btn-default'");?>
-          <?php echo html::a($this->createLink('company', 'setBasic'), $lang->admin->shortcuts->company, "class='btn btn-default'");?>
-          <?php echo html::a($this->createLink('company', 'setcontact'), $lang->admin->shortcuts->contact, "class='btn btn-default'")?>  
-        </div>
-      </div>
-    </div>
+    <?php endforeach;?>
   </div>
 </div>
 <?php include '../../common/view/footer.admin.html.php';?>
