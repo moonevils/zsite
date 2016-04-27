19d414bf1171506b55ce534e955027a458739f4f
daitingting <daitingting@xirangit.com>
2016-04-27 10:50:08 +0800

    * Change language when login.

--- system/module/user/js/login.js
+++ system/module/user/js/login.js
@@ -1,3 +1,11 @@
+$(document).ready(function()
+{
+    $("#langs li > a").click(function() 
+    {
+        selectLang($(this).data('value'));
+    });
+})
+
 /* Keep session random valid. */
 needPing = true;
 $('#submit').click(function()
