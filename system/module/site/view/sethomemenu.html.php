8751a5bb2e3a5b0a0960fcff149660e2f729827c
daitingting <daitingting@xirangit.com>
2016-04-27 10:25:32 +0800

    * Set home menu.

---
+++ system/module/site/view/sethomemenu.html.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The setbasic view file of site module of chanzhiEPS.
+ *
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPLV1.2 (http://zpl.pub/page/zplv12.html)
+ * @author      xiying Guang <guanxiying@xirangit.com>
+ * @package     site
+ * @version     $Id$
+ * @link        http://www.chanzhi.org
+ */
+?>
+<?php include '../../common/view/header.admin.html.php';?>
+<div class='panel'>
+  <div class='panel-heading'><strong><i class='icon-globe'></i> <?php echo $lang->site->setHomeMenu;?></strong></div>
+  <div class='panel-body'>
+    <form method='post' id='ajaxForm' class='form-inline'>
+      <table class='table table-form'>
+        <tr>
+          <th class='w-100px'><?php echo $lang->site->homeMenus;?></th>
+          <td><?php echo html::checkbox('homeMenus', $lang->site->menus, $this->config->menus->home);?></td>
+        </tr>
+        <tr>
+          <th></th>
+          <td><?php echo html::submitButton();?></td>
+        </tr>
+      </table>
+    </form>
+  </div>
+</div>
+<?php include '../../common/view/footer.admin.html.php';?>
