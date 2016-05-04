322de488ae5458458f515ecb2fa6478ff189fb2e
guanxiying <daitingting@xirangit.com>
2016-05-04 09:54:16 +0800

    * Add admin check.

--- system/module/ui/js/edittemplate.js
+++ system/module/ui/js/edittemplate.js
@@ -1,5 +1,15 @@
 $(document).ready(function()
 {
+    $.setAjaxForm('#editForm', function(response) 
+    {
+        if(response.result == 'fail')
+        {
+            bootbox.alert(response.warning);
+        }
+    });
+
+    $.setAjaxLoader('.okFile', '#ajaxModal');
+
     $('#resetBtn').click(function()
     {
        $('#content').val($('#rawContent').val());
