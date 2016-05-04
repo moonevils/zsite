7e0881a869209bb380273c0daeaa674d058649f9
daitingting <daitingting@xirangit.com>
2016-05-04 15:06:55 +0800

    * Remove unused codes.

--- system/module/nav/view/admin.html.php
+++ system/module/nav/view/admin.html.php
@@ -18,10 +18,7 @@
     <ul id='typeNav' class='nav nav-tabs'>
       <?php if($this->config->site->type != 'blog'):?>
       <li data-type='internal' <?php echo $type == 'desktop_top' ? "class='active'" : '';?>>
-        <?php echo html::a(helper::createLink('nav', 'admin', "type=desktop_top"), $lang->nav->desktop_top);?>
-      </li>
-      <li data-type='internal' <?php echo $type == 'desktop_bottom' ? "class='active'" : '';?>>
-        <?php echo html::a(helper::createLink('nav', 'admin', "type=desktop_bottom"), $lang->nav->desktop_bottom);?>
+        <?php echo html::a(helper::createLink('nav', 'admin', "type=desktop_top"), $lang->nav->desktop);?>
       </li>
       <li data-type='internal' <?php echo $type == 'mobile_top' ? "class='active'" : '';?>>
         <?php echo html::a(helper::createLink('nav', 'admin', "type=mobile_top"), $lang->nav->mobile_top);?>
