bdbbe90c468a5a803b94439df028a586888a2d89
guanxiying <daitingting@xirangit.com>
2016-05-06 14:45:11 +0800

    * Print cart info.

--- system/module/user/control.php
+++ system/module/user/control.php
@@ -1134,8 +1134,16 @@ class user extends control
         $this->display();
     }
 
+    /**
+     * Print topbar.
+     * 
+     * @access public
+     * @return void
+     */
     public function printTopbar()
     {
+        $cartInfo = $this->loadModel('cart')->getCount();
+        if($cartInfo) echo "<span class='text-center text-middle' id='cartBox'>{$cartInfo}</span>";
         commonModel::printTopBar();
         commonModel::printLanguageBar();
         die();
