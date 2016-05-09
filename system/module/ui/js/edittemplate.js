c16fa7ae5e3ffe48c3591164286a492a5b9e5363
Catouse <catouse@gmail.com>
2016-05-09 17:33:24 +0800

    * improve UI of 'ui/edittemplate'.

--- system/module/ui/js/edittemplate.js
+++ system/module/ui/js/edittemplate.js
@@ -35,5 +35,21 @@ $(document).ready(function()
     $('.folder-menu').find('.panel-heading').click(function()
     {
         $(this).next().toggle();
-    })
+    });
+
+    var extraHeight = $('#mainNavbar').outerHeight() + $('#menu').outerHeight() + $('#mainPanel > .panel-heading').outerHeight() + $('#mainPanel > .panel-footer').outerHeight() + 80;;
+    var resizeEditors = function()
+    {
+        $('.codeeditor').each(function()
+        {
+            var options = $(this).data();
+            var height = Math.max(100, $(window).height() - extraHeight);
+            $('#' + options.editorId).height(height);
+            options.editor.resize();
+            console.log(options, height);
+        });
+    };
+
+    $(window).on('resize', resizeEditors);
+    resizeEditors();
 });
