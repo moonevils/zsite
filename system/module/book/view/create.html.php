a622d5af83c85d6b785245b258ccc6174878054f
daitingting <daitingting@xirangit.com>
2016-05-09 16:08:25 +0800

    * Adjust left menu for book.

--- system/module/book/view/create.html.php
+++ system/module/book/view/create.html.php
@@ -11,48 +11,49 @@
  */
 ?>
 <?php include '../../common/view/header.admin.html.php'; ?>
-<div class='panel'>
-  <div class='panel-heading'><strong><i class='icon-plust'></i> <?php echo $lang->book->createBook;?></strong></div>
-  <div class='panel-body'>
-    <form id='ajaxForm' method='post' class='form-inline'>
-      <table class='table table-form'>
-        <tr>
-          <th class='w-100px'><?php echo $lang->book->author;?></th>
-          <td><?php echo html::input('author', $app->user->realname, "class='form-control'");?></td>
-        </tr>
-        <tr>
-          <th><span><?php echo $lang->book->title;?></span></th>
-          <td>
-            <div class='required required-wrapper'></div>
-            <?php echo html::input('title', '', 'class=form-control');?>
-          </td>
-        </tr>
-        <tr>
-          <th><span><?php echo $lang->book->alias;?></span></th>
-          <td>
-            <div class='required required-wrapper'></div>
-            <div class='input-group'>
-              <span class='input-group-addon'>http://<?php echo $this->server->http_host . $config->webRoot?>book/</span>
-              <?php echo html::input('alias', '', "class='form-control' placeholder='{$lang->alias}'");?>
-              <span class='input-group-addon'>.html</span>
-            </div>
-          </td>
-        </tr>
-        <tr>
-          <th><?php echo $lang->book->keywords;?></th>
-          <td><?php echo html::input('keywords', '', 'class=form-control');?></td>
-        </tr>
-        <tr>
-          <th><?php echo $lang->book->summary;?></th>
-          <td><?php echo html::textarea('summary', '', "class='form-control' rows='3'");?></td>
-        </tr>
-        <tr>
-          <th></th><td><?php echo html::submitButton();?></td>
-        </tr>
-      </table>
-    </form>
+<?php include './side.html.php';?>
+<div class='col-md-10'>
+  <div class='panel'>
+    <div class='panel-heading'><strong><i class='icon-plust'></i> <?php echo $lang->book->createBook;?></strong></div>
+    <div class='panel-body'>
+      <form id='ajaxForm' method='post' class='form-inline'>
+        <table class='table table-form'>
+          <tr>
+            <th class='w-100px'><?php echo $lang->book->author;?></th>
+            <td><?php echo html::input('author', $app->user->realname, "class='form-control'");?></td>
+          </tr>
+          <tr>
+            <th><span><?php echo $lang->book->title;?></span></th>
+            <td>
+              <div class='required required-wrapper'></div>
+              <?php echo html::input('title', '', 'class=form-control');?>
+            </td>
+          </tr>
+          <tr>
+            <th><span><?php echo $lang->book->alias;?></span></th>
+            <td>
+              <div class='required required-wrapper'></div>
+              <div class='input-group'>
+                <span class='input-group-addon'>http://<?php echo $this->server->http_host . $config->webRoot?>book/</span>
+                <?php echo html::input('alias', '', "class='form-control' placeholder='{$lang->alias}'");?>
+                <span class='input-group-addon'>.html</span>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <th><?php echo $lang->book->keywords;?></th>
+            <td><?php echo html::input('keywords', '', 'class=form-control');?></td>
+          </tr>
+          <tr>
+            <th><?php echo $lang->book->summary;?></th>
+            <td><?php echo html::textarea('summary', '', "class='form-control' rows='3'");?></td>
+          </tr>
+          <tr>
+            <th></th><td><?php echo html::submitButton();?></td>
+          </tr>
+        </table>
+      </form>
+    </div>
   </div>
 </div>
-
-
 <?php include '../../common/view/footer.admin.html.php';?>
--- system/module/book/view/setting.html.php
+++ system/module/book/view/setting.html.php
@@ -11,30 +11,33 @@
  */
 ?>
 <?php include '../../common/view/header.admin.html.php';?>
-<div class='panel'>
-  <div class='panel-heading'><strong><i class='icon-cog'></i> <?php echo $lang->book->setting;?></strong></div>
-  <form method='post' id='ajaxForm' class='form-inline'>
-    <table class='table table-form'>
-      <tr>
-        <th class='w-80px'><?php echo $lang->book->index;?></th>
-        <td class='w-p40'><?php echo html::select('index', $books, isset($this->config->book->index) ? $this->config->book->index : $firstBook, "class='form-control'");?></td>
-        <td></td>
-      </tr>
-      <tr>
-        <th><?php echo $lang->book->fullScreen;?></th>
-        <td><?php echo html::radio('fullScreen', $lang->book->fullScreenList, isset($this->config->book->fullScreen) ? $this->config->book->fullScreen : 0, "class='checkbox'");?></td>
-        <td></td>
-      </tr>
-      <tr class='chapter <?php echo !empty($this->config->book->fullScreen) ? 'hide' : '';?>'>
-        <th><?php echo $lang->book->chapterList;?></th>
-        <td><?php echo html::select('chapter', $lang->book->chapterTypeList, isset($this->config->book->chapter) ? $this->config->book->chapter : '', "class='form-control'");?></td>
-        <td></td>
-      </tr>
-      <tr>
-        <th></th>
-        <td colspan='2'><?php echo html::submitButton();?></td>
-      </tr>
-    </table>
-  </form>
+<?php include './side.html.php';?>
+<div class='col-md-10'>
+  <div class='panel'>
+    <div class='panel-heading'><strong><i class='icon-cog'></i> <?php echo $lang->book->setting;?></strong></div>
+    <form method='post' id='ajaxForm' class='form-inline'>
+      <table class='table table-form'>
+        <tr>
+          <th class='w-80px'><?php echo $lang->book->index;?></th>
+          <td class='w-p40'><?php echo html::select('index', $books, isset($this->config->book->index) ? $this->config->book->index : $firstBook, "class='form-control'");?></td>
+          <td></td>
+        </tr>
+        <tr>
+          <th><?php echo $lang->book->fullScreen;?></th>
+          <td><?php echo html::radio('fullScreen', $lang->book->fullScreenList, isset($this->config->book->fullScreen) ? $this->config->book->fullScreen : 0, "class='checkbox'");?></td>
+          <td></td>
+        </tr>
+        <tr class='chapter <?php echo !empty($this->config->book->fullScreen) ? 'hide' : '';?>'>
+          <th><?php echo $lang->book->chapterList;?></th>
+          <td><?php echo html::select('chapter', $lang->book->chapterTypeList, isset($this->config->book->chapter) ? $this->config->book->chapter : '', "class='form-control'");?></td>
+          <td></td>
+        </tr>
+        <tr>
+          <th></th>
+          <td colspan='2'><?php echo html::submitButton();?></td>
+        </tr>
+      </table>
+    </form>
+  </div>
 </div>
 <?php include '../../common/view/footer.admin.html.php';?>
