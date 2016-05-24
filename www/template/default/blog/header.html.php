89e60b0ebe29d79e235c4a7bfbf15d1ea97f1fe5
sunjinliang <sunjinliang@cnezsoft.com>
2016-05-24 11:23:42 +0800

    * Edit the bug of the address of logo when clicked in blog

--- www/template/default/blog/header.html.php
+++ www/template/default/blog/header.html.php
@@ -132,10 +132,10 @@ if(!empty($baseCustom[$template][$theme]['js'])) js::execute($baseCustom[$templa
         <?php $logo = isset($logoSetting->$template->themes->$theme) ? $logoSetting->$template->themes->$theme : (isset($logoSetting->$template->themes->all) ? $logoSetting->$template->themes->all : false);?>
         <?php if($logo):?>
         <div id='siteLogo' data-ve='logo'>
-          <?php echo html::a($this->config->webRoot, html::image($logo->webPath, "class='logo' title='{$this->config->company->name}'"));?>
+          <?php echo html::a(helper::createLink('index'), html::image($logo->webPath, "class='logo' alt='{$this->config->company->name}' title='{$this->config->company->name}'"));?>
         </div>
         <?php else: ?>
-        <div id='siteName' data-ve='logo'><h2><?php echo html::a($this->config->webRoot, $this->config->site->name);?></h2></div>
+        <div id='siteName' data-ve='logo'><h2><?php echo html::a(helper::createLink('index'), $this->config->site->name);?></h2></div>
         <?php endif;?>
       </div>
     </div>
