90894da8126c0adac8337105fe454cca05efe669
sunjinliang <sunjinliang@cnezsoft.com>
2016-05-24 11:21:27 +0800

    * Edit the bug of the address of logo when clicked

--- www/template/default/block/header.default.html.php
+++ www/template/default/block/header.default.html.php
@@ -20,10 +20,10 @@
     <div class="wrapper">
       <?php if($logo):?>
       <div id='siteLogo' data-ve='logo'>
-        <?php echo html::a($this->config->webRoot, html::image($logo->webPath, "class='logo' title='{$this->config->company->name}'"));?>
+        <?php echo html::a(helper::createLink('index'), html::image($logo->webPath, "class='logo' alt='{$this->config->company->name}' title='{$this->config->company->name}'"));?>
       </div>
       <?php else: ?>
-      <div id='siteName' data-ve='logo'><h2><?php echo html::a($this->config->webRoot, $this->config->site->name);?></h2></div>
+      <div id='siteName' data-ve='logo'><h2><?php echo html::a(helper::createLink('index'), $this->config->site->name);?></h2></div>
       <?php endif;?>
       <div id='siteSlogan' data-ve='slogan'><span><?php echo $this->config->site->slogan;?></span></div>
     </div>
