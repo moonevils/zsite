cb11afc93b00629e526b4fcfaaa395e30fb6f775
guanxiying <guanxiying@xirangit.com>
2016-05-25 13:54:40 +0800

    * Hide empty lang var.

--- system/framework/router.class.php
+++ system/framework/router.class.php
@@ -789,7 +789,7 @@ class router
             else
             {
                 $flipedLangs = array_flip($this->config->langsShortcuts);
-                if($this->config->requestType == 'GET' and isset($_GET[$this->config->langVar])) $this->clientLang = $flipedLangs[$_GET[$this->config->langVar]];
+                if($this->config->requestType == 'GET' and !empty($_GET[$this->config->langVar])) $this->clientLang = $flipedLangs[$_GET[$this->config->langVar]];
                 if($this->config->requestType != 'GET')
                 {
                     $pathInfo = $this->getPathInfo();
