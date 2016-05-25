25160a1feb5bd5734f73bbd99f95cca9ed40df71
guanxiying <guanxiying@xirangit.com>
2016-05-25 10:17:48 +0800

    * Fix active article style.

--- www/template/default/book/js/common.js
+++ www/template/default/book/js/common.js
@@ -1,11 +1,10 @@
 $(document).ready(function()
 {
     $('.nav-system-book').addClass('active');
-
+    $('#article' + v.objectID).addClass('active');
     if(v.fullScreen)
     {
         $('html, body').css('height', '100%');
-        $('#article' + v.objectID).css('font-weight', 'bold');
 
         curPos = sessionStorage.getItem('curPos');
         if(curPos) $('.fullScreen-catalog').animate({scrollTop: curPos}, 0);
