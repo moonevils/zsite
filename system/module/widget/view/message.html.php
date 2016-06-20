4035821eafaa88b54226c06a043ca037e8d0b66a
sunjinliang <sunjinliang@cnezsoft.com>
2016-06-20 15:54:50 +0800

    * Edit the link of message to reply this message in the block of message

--- system/module/widget/view/message.html.php
+++ system/module/widget/view/message.html.php
@@ -6,8 +6,12 @@ $messages = $this->message->getListForWidget($limit);
 <table class='table table-data table-hover table-fixed'>
   <?php foreach($messages as $message):?>
   <tr>
-    <td><?php echo $message->from . $lang->colon . html::a($message->objectViewURL, $message->content, "target='_blank'");?></td>
-    <td class='w-30px text-center'><?php echo html::a(helper::createLink('message', 'reply', "id={$message->id}"), "<i class='icon icon-reply'> </i>", "data-toggle='modal'");?></td>
-  </tr>
+    <td>
+    <?php
+    $href = helper::createLink('message', 'reply', "id={$message->id}");
+    echo $message->from . $lang->colon . html::a($href, $message->content, "data-toggle='modal'");
+    ?>
+    </td>
+</tr>
   <?php endforeach;?>
 </table>
