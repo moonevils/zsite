0f125f1f38ba8207efb1ce5ff1dbf69b954752f4
guanxiying <guanxiying@xirnagit.com>
2016-06-23 13:29:55 +0800

    * Format codes.

--- system/module/widget/view/wechatmessage.html.php
+++ system/module/widget/view/wechatmessage.html.php
@@ -1,17 +1,16 @@
 <?php
-$this->loadModel('wechat');
-$limit  = zget($widget->params, 'limit', 10);
-$messages = $this->wechat->getListForWidget($limit);
+$limit    = zget($widget->params, 'limit', 10);
+$messages = $this->loadModel('wechat')->getListForWidget($limit);
 ?>
 <table class='table table-data table-hover table-fixed'>
   <?php foreach($messages as $message):?>
   <tr>
     <td>
-    <?php
-    $href = helper::createLink("wechat", "reply", "message={$message->id}");  
-    echo $message->fromUserName . $lang->colon . html::a($href, $message->content, "data-toggle='modal'");
-    ?>
+      <?php
+      $href = helper::createLink('wechat', 'reply', "message={$message->id}");  
+      echo $message->fromUserName . $lang->colon . html::a($href, $message->content, "data-toggle='modal'");
+      ?>
     </td>
-</tr>
+  </tr>
   <?php endforeach;?>
 </table>
