93a04a038e7f710ae36ab83578c776399c930de4
sunjinliang <sunjinliang@cnezsoft.com>
2016-07-08 11:30:03 +0800

    * Edit js file of edittemplate

--- system/module/ui/js/edittemplate.js
+++ system/module/ui/js/edittemplate.js
@@ -24,14 +24,13 @@ $(document).ready(function()
     });
 
     btn = $('.btn-file.active');
-    file = btn.parents('.panel').find('strong').html() + ' / ' + btn.html();
+    file = btn.parents('.panel').find('span').html() + ' / ' + btn.html();
     $('#fileName').prepend(file);
 
     $('.panel-folder').each(function()
     {
         $(this).toggle($(this).find('a.active').size() > 0);
     });
-
     $('.folder-menu').find('.panel-heading').click(function()
     {
         $(this).next().toggle();
