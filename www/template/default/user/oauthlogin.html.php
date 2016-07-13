c9dd465108baff662b446eedce880b21632bd43a
sunjinliang <sunjinliang@cnezsoft.com>
2016-07-13 16:59:01 +0800

    * Edit the style of the login and register page

--- www/template/default/user/oauthlogin.html.php
+++ www/template/default/user/oauthlogin.html.php
@@ -1,31 +1,28 @@
-<?php 
-foreach($lang->user->oauth->providers as $providerCode => $providerName) $providerConfig[$providerCode] = isset($config->oauth->$providerCode) ? json_decode($config->oauth->$providerCode) : '';
-if(!empty($providerConfig[$providerCode]->clientID) or !empty($this->config->site->yangcong)):
+<?php
+$yangcongConfig = json_decode(zget($this->config->site, 'yangcong', array()));
+foreach($lang->user->oauth->providers as $providerCode => $providerName)
+{
+    if(isset($config->oauth->$providerCode)) $providerConfig[$providerCode] = json_decode($config->oauth->$providerCode);
+}
+if(!empty($providerConfig['qq']->clientID) or !empty($providerConfig['sina']->clientID) or !empty($yangcongConfig->appID)):
 ?>
-  <div class='col-md-6'>
-    <div class='panel panel-pure'>
-      <div class='panel-heading'><strong><?php echo $lang->user->oauth->lblWelcome;?></strong></div>
-      <div class='panel-body'>
-        <?php 
-        foreach($lang->user->oauth->providers as $providerCode => $providerName) 
-        {
-            $providerConfig = isset($config->oauth->$providerCode) ? json_decode($config->oauth->$providerCode) : '';
-            if(empty($providerConfig->clientID)) continue;
-            $params = "provider=$providerCode&fingerprint=fingerprintval";
-            if($referer and !strpos($referer, 'login') and !strpos($referer, 'oauth')) $params .= "&referer=" . helper::safe64Encode($referer);
-            if($providerCode == 'qq')
-                echo html::a(inlink('oauthLogin', $params), "<img src='../../../theme/default/default/images/main/qqlogin.png'>", "class='btn btn-default btn-oauth btn-lg btn-block login-qq'");
-            else
-                echo html::a(inlink('oauthLogin', $params), "<i class='icon-{$providerCode} icon'></i> " . $providerName, "class='btn btn-default btn-oauth btn-lg btn-block btn-{$providerCode}'");
-        }
-        ?>
-        <?php if(!empty($this->config->site->yangcong)) echo html::a(helper::createLink('yangcong', 'qrcode', "referer=" . helper::safe64Encode($referer)), "<i class='icon icon-yangcong icon-lg'></i>{$lang->user->yangcongLogin}", "class='btn btn-lg btn-block btn-default btn-yangcong btn-oauth' data-toggle='modal'");?>
-      </div>
-    </div>
-  </div>
-  <div class='col-md-6'>
-<?php else:?>
-  <div class='col-md-12'>
+<span class='login-images'>
+<?php
+echo "<span class='login-heading'>" . $lang->user->oauth->lblOtherLogin . "</span>";
+foreach($lang->user->oauth->providers as $providerCode => $providerName) 
+{
+    $providerConfig = isset($config->oauth->$providerCode) ? json_decode($config->oauth->$providerCode) : '';
+    if(empty($providerConfig->clientID)) continue;
+    $params = "provider=$providerCode&fingerprint=fingerprintval";
+    if($referer and !strpos($referer, 'login') and !strpos($referer, 'oauth')) $params .= "&referer=" . helper::safe64Encode($referer);
+    echo html::a(inlink('oauthLogin', $params), "<img class='login-image " . $providerCode . "' src='../../../theme/default/default/images/main/" . $providerCode . "login.png'>");  
+}
+if(zget($yangcongConfig, 'appID', 0)) 
+{
+    echo html::a(helper::createLink('yangcong', 'qrcode', "referer=" . helper::safe64Encode($referer)), "<img class='login-image' src='../../../theme/default/default/images/main/yangconglogin.png'>", "data-toggle='modal'");
+}
+?>
+</span>
 <?php endif;?>
 <script>
 $().ready(function()
@@ -35,5 +32,5 @@ $().ready(function()
         fingerprint = getFingerprint();
         $(this).attr('href', $(this).attr('href').replace('fingerprintval', fingerprint) )
     })
-})
+});
 </script>
