9acbd92ca99a1e7e020ab1eb5389d339548df639
sunjinliang <sunjinliang@cnezsoft.com>
2016-09-29 08:23:14 +0800

    * Adjust the setbasic of site

---
+++ system/module/site/js/setsystem.js
@@ -0,0 +1,58 @@
+$(document).ready(function()
+{
+    /* Change set lang imput. */
+    $('input[type=checkbox]').change(function()
+    {
+        if($('input[type=checkbox][value=zh-cn]').prop('checked') && $('input[type=checkbox][value=zh-tw]').prop('checked'))
+        {
+            $('#twTR').show();
+        }
+        else
+        {
+            $('#twTR').hide();
+        }
+
+        $('input[type=checkbox]').each(function()
+        {
+            checked = $(this).prop('checked');
+            lang = $(this).val();
+            if(!checked)
+            {
+                $('#defaultLang').find('[value=' + lang  + ']').prop('disabled', true);
+            }
+            else
+            {
+                $('#defaultLang').find('[value=' + lang  + ']').prop('disabled', false);
+            }
+        })
+    });
+
+    $('#requestType').change(function()
+    {
+        if($(this).find('option:selected').val() != 'PATH_INFO')
+        {
+            $.ajax(
+            {
+                type: 'get',
+                url: '/index',
+                dataType: 'json',
+                success: function(data){return false;},  
+                error: function(data)
+                {
+                    if(data.status == '200') 
+                    {
+                        $('#requestTypeTip').fadeIn();
+                    }
+                }
+            });
+        }
+        else
+        {
+            $('#requestTypeTip').hide();
+        }
+
+    })
+
+    $('input[type=checkbox][id*=lang]').change();
+    $('#requestType').change();
+})
