0703623d8c6b887d0dae742493469f8ccdbff968
Catouse <catouse@gmail.com>
2016-09-29 11:37:29 +0800

    * fix login error.

--- www/template/mobile/user/login.front.html.php
+++ www/template/mobile/user/login.front.html.php
@@ -11,7 +11,6 @@
  */
 ?>
 <?php include $this->loadModel('ui')->getEffectViewFile('mobile', 'common', 'header'); ?>
-<?php js::import($this->config->webRoot . 'js/fingerprint/fingerprint.js');?>
 <hr class='space'>
 <div class='panel-section'>
   <?php include TPL_ROOT . 'user/oauthlogin.html.php';?>
@@ -36,4 +35,20 @@
   </div>
 </div>
 <?php include TPL_ROOT . 'common/form.html.php'; ?>
+<script>
+$(function()
+{
+    $('#loginForm').ajaxform(
+    {
+        onSubmit: function(data)
+        {
+            var account = data['account'];
+            var password = data['password'];
+            var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+            if(!reg.test(account)) password = md5(md5(md5(password) + account) + v.random);
+            data['password'] = password;
+        }
+    });
+});
+</script>
 <?php include $this->loadModel('ui')->getEffectViewFile('mobile', 'common', 'footer');?>
