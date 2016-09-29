85ba50684248f9659d0089ce18949ca87c1b22b7
guanxiying <guanxiying@xirnagit.com>
2016-09-29 14:58:01 +0800

    * Add upgrade 5.3.4 sql.

---
+++ system/db/upgrade5.3.4.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `eps_farm` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `url` char(30) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `admin` char(30) NOT NULL,
+  `password` char(32) NOT NULL,
+  `private` char(32) NOT NULL,
+  `lang` char(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `lang` (`lang`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
