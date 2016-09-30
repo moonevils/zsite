d7e85ddabbe9d17f6e678cf7864538626d646a44
guanxiying <guanxiying@xirnagit.com>
2016-09-30 12:27:20 +0800

    * Remove farm table.

--- system/db/upgrade5.3.4.sql
+++ system/db/upgrade5.3.4.sql
@@ -1,12 +1,1 @@
-CREATE TABLE `eps_farm` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `url` char(30) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `admin` char(30) NOT NULL,
-  `password` char(32) NOT NULL,
-  `private` char(32) NOT NULL,
-  `lang` char(30) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `lang` (`lang`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 ALTER TABLE `eps_order` change status status enum('normal','canceled','finished','deleted','expired') NOT NULL DEFAULT 'normal';
