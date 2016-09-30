d48263d729cee7e40217d6ce5150ed5ef2d86735
guanxiying <guanxiying@xirnagit.com>
2016-09-30 16:55:22 +0800

    * Save config to my.php .

--- system/module/upgrade/control.php
+++ system/module/upgrade/control.php
@@ -48,7 +48,7 @@ class upgrade extends control
         $this->view->title = $this->lang->upgrade->backup;
         $this->view->db    = $this->config->db;
         $this->view->slidePath = $this->app->getDataRoot() . 'slides';
-        $this->view->customFile =  $this->app->getConfigRoot() . 'custom.php';
+        $this->view->customFile =  $this->app->getConfigRoot() . 'my.php';
         if(!is_dir($this->view->slidePath)) mkdir($this->view->slidePath, 0777, true);
 
         $this->view->createSlidePath   = !is_writeable($this->view->slidePath);
