0c973e9286c00c481825bcfaa104972edcdf9bea
guanxiying <guanxiying@xirnagit.com>
2016-10-11 09:28:30 +0800

    * Fix getClientDevice function.

--- system/framework/router.class.php
+++ system/framework/router.class.php
@@ -75,27 +75,32 @@ class router extends baseRouter
      */
     public function setClientDevice()
     {
-        $device = 'desktop';
-        if(strpos('mobile,desktop', $this->cookie->device) !== false) $device = $this->cookie->device;
+        if(strpos('mobile,desktop', $this->cookie->device) === false) 
+        {
+            $mobile = new mobile();
+            $device = ($mobile->isMobile() and !$mobile->isTablet()) ? 'mobile' : 'desktop';
+        }
+        else
+        {
+            $device = $this->cookie->device;
+        }
 
         if(RUN_MODE == 'admin')
         {
-            $device = $this->session->device ? $this->session->device : 'desktop';
+            if(strpos('mobile,desktop', $this->session->device) !== false)  $device = $this->session->device;
         }
         elseif(RUN_MODE == 'front')
         {
-            if(isset($_COOKIE['visualDevice'])) $device = $_COOKIE['visualDevice'];
+            if(isset($_COOKIE['visualDevice']) and strpos('mobile,desktop', $_COOKIE['visualDevice']) !== false) $device = $_COOKIE['visualDevice'];
         }
         
         $pathInfo = $this->getPathInfo();
         $dotPos   = strrpos($pathInfo, '.');
         $viewType = substr($pathInfo, $dotPos + 1);
         if($viewType == 'mhtml') $device = 'mobile';
-
         $this->clientDevice = $device;
         $this->cookie->set('device', $this->clientDevice);
         $this->cookie->set('device', $this->clientDevice);
-        parent::setClientDevice();
     }
 
     /**
