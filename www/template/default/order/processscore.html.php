8f6295cf09d8af1c5800ae7dbacc1aa73513d279
sunjinliang <sunjinliang@cnezsoft.com>
2016-10-24 13:24:01 +0800

    * Adjust style

--- www/template/default/order/processscore.html.php
+++ www/template/default/order/processscore.html.php
@@ -3,14 +3,8 @@
 <?php if($result):?>
 <div class='container' id='payResult'>
   <div class='modal-dialog w-450px'> 
-    <div class='alert alert-success'>
-      <div class='content'>
-        <i class='icon icon-ok'> </i>
-        <?php echo $lang->order->paidSuccess;?>
-        <span style='padding-left:10px;'><?php echo html::a($this->createLink('user', 'score'), "<i class='icon icon-gift'> </i>" . $lang->score->details, "class='btn btn-xs btn-primary'");?></span>
-      </div>
-    </div>
-  </div>
+  <div class='modal-body'><div class='alert alert-success text-center'><h4><i class="text-success icon-ok-sign"></i> <?php echo $lang->order->paidSuccess;?></h4></div></div>
+  <div class='modal-footer'><?php echo html::a(helper::createLink('user', 'score'), $lang->score->details, "class='btn btn-success'");?></div>
 </div>
 <?php else:?>
 <h3 class='text-center text-danger'><?php echo $lang->score->payFail;?></h3>
