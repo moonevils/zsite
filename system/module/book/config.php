a52594b93d0c69cf6eff687249812899a7c0f12a
sunjinliang <sunjinliang@cnezsoft.com>
2016-11-10 14:17:07 +0800

    * Adjust code

--- system/module/book/config.php
+++ system/module/book/config.php
@@ -4,6 +4,7 @@ $config->book->require->book = 'title, alias';
 $config->book->require->node = 'title';
 
 $config->book->editor = new stdclass();
-$config->book->editor->edit = array('id' => 'content', 'tools' => 'full');
+$config->book->editor->edit   = array('id' => 'content', 'tools' => 'full');
+$config->book->editor->create = array('id' => 'content', 'tools' => 'full');
 
 $config->book->chapter = 'left';
