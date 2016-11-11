d73e6ac2d162fcca18d6131c6819d8e7f461ce10
zhubaoxin <zhubaoxin@qq.com>
2016-11-11 09:05:08 +0800

    * Add the read js of book

--- www/template/default/book/js/read.js
+++ www/template/default/book/js/read.js
@@ -6,4 +6,42 @@ $(document).ready(function()
         selector: "[data-toggle=tooltip]",
         container: "body"
     });  
+    //scroll
+    function yrScroll(){
+        var headerHeight = $("#book").offset().top;
+        var listTitleHeight = $(".book-catalog .panel-heading").height();
+        //init
+        $(".book-catalog").css({
+            "max-height":"600px",
+            "overflow":"auto"
+        });
+        var listScrollTop =  $(".books .active").position().top;
+        var listMoveSize = listScrollTop > ( $(".book-catalog").height() - listTitleHeight ) / 2 ? listScrollTop : 0;
+        var scrollMoveSize = listMoveSize / $(".books").height(); 
+        $(".book-catalog").scrollTop(
+            $(".book-catalog .books").height() * scrollMoveSize - ( $(".book-catalog").height() / 2 - listTitleHeight )
+        );
+        console.log(listMoveSize)
+        console.log($(".book-catalog .book").height() * scrollMoveSize)
+        //scroll event
+        $(document).on("scroll", function (){
+             //minHeight
+             $(".page-wrapper").css({"min-height":$(".book-catalog").height()})
+             if($(document).scrollTop() > headerHeight ){
+                $(".book-catalog").css({
+                    "position":"fixed",
+                    "top":"0",
+                    "width":"276"
+                });
+             }else if( $(document).scrollTop() < headerHeight ){
+                $(".book-catalog").css({
+                    "position":"relative"
+                });
+            }
+        });
+    };
+    yrScroll();
+    //scroll end
+
+
 });
