c735833ba6f164f0e3d3a9033cc4b0b09a86823c
sunjinliang <sunjinliang@cnezsoft.com>
2016-11-11 17:17:55 +0800

    * Add the page to cache

--- system/config/cache.php
+++ system/config/cache.php
@@ -6,7 +6,7 @@ $config->cache->expired   = 86400;
 $config->cache->cachePage = 'open';
 
 $config->cache->type = 'file';
-$config->cache->cachedPages = 'index.index,article.browse,product.browse,book.browse,book.index,';
+$config->cache->cachedPages = 'index.index,article.browse,product.browse,product.view,page.view,blog.view,book.browse,book.index,book.read,';
 
 $config->cache->file = new stdclass();
 $config->cache->file->expired = $config->cache->expired;
@@ -26,18 +26,18 @@ $config->cache->relation[TABLE_CONFIG]['blocks'] = '/';
 $config->cache->relation[TABLE_CONFIG]['pages']  = '/';
 
 $config->cache->relation[TABLE_ARTICLE]['blocks'] = 'latestarticle,hotarticle,latestblog,pagelist,';
-$config->cache->relation[TABLE_ARTICLE]['pages']  = 'article.browse,blog.index';
+$config->cache->relation[TABLE_ARTICLE]['pages']  = 'article.browse,blog.index,blog.view';
 
 $config->cache->relation[TABLE_CATEGORY]['blocks'] = 'latestarticle,hotarticle,latestblog,pagelist,articletree,producttree,blogtree,slide';
-$config->cache->relation[TABLE_CATEGORY]['pages']  = 'article.browse,blog.index,product.browse,index.index';
+$config->cache->relation[TABLE_CATEGORY]['pages']  = 'article.browse,blog.index,blog.view,product.browse,index.index';
 
 $config->cache->relation[TABLE_PRODUCT]['blocks'] = 'latestproduct,hotproduct,featuredproduct,';
-$config->cache->relation[TABLE_PRODUCT]['pages']  = 'product.browse';
+$config->cache->relation[TABLE_PRODUCT]['pages']  = 'product.browse,product.view';
 
 $config->cache->relation[TABLE_FILE]['blocks'] = 'latestarticle,hotarticle,latestblog,pagelist,articletree,producttree,blogtree,slide,featuredproduct';
-$config->cache->relation[TABLE_FILE]['pages']  = 'article.browse,blog.index,product.browse,index.index,blog.index,';
+$config->cache->relation[TABLE_FILE]['pages']  = 'article.browse,blog.index,blog.view,product.browse,product.view,index.index,blog.index,';
 
-$config->cache->relation[TABLE_BOOK]['pages']  = 'book.browse,book.index';
+$config->cache->relation[TABLE_BOOK]['pages']  = 'book.browse,book.index,book.read';
 
 $config->cache->relation[TABLE_THREAD]['blocks'] = 'latestproduct,hotproduct,';
 
