992787873ca15de0664eb6de326a0456d0e50085
guanxiying <guanxiying@xirnagit.com>
2016-11-14 14:58:01 +0800

    * Fix mobdule menu of tree browse.

--- system/module/tree/model.php
+++ system/module/tree/model.php
@@ -750,6 +750,7 @@ class treeModel extends model
     {
         $menuGroup = zget($this->config->tree->menuGroups, $type);
         unset($this->lang->tree->menu);
+        if(isset($this->lang->$menuGroup) and isset($this->lang->{$menuGroup}->menu)) $this->lang->tree->menu = $this->lang->{$menuGroup}->menu;
         $this->lang->menuGroups->tree = $menuGroup;
     }
 }
