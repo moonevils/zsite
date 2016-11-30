974c6be3ae058968bbb88d5e8d93dc217bf4cafb
guanxiying <guanxiying@xirnagit.com>
2016-11-30 11:23:33 +0800

    * rename errors.

--- system/module/error/lang/en.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lang->error = new stdclass();
-$lang->error->pageNotFound    = "Page not found.";
-$lang->error->articleCategory = 'Article Category';
-$lang->error->productCategory = 'Product Category';
-$lang->error->blogCategory    = 'Blog Category';
-$lang->error->searchTip       = 'Please continue browsing our website. You can use Search to loof for';
