5e943909fde1e3d3aaa28c8a778309e7e24fd5ea
sunjinliang <sunjinliang@cnezsoft.com>
2016-12-07 15:03:07 +0800

    * Adjust lang

--- system/module/file/lang/zh-cn.php
+++ system/module/file/lang/zh-cn.php
@@ -74,4 +74,4 @@ $lang->file->rebuildThumbs    = "已完成 %s";
 $lang->file->noFlashTip       = "Flash插件被禁用，请手动复制";
 
 $lang->file->updateInvalidFiles = '更新列表';
-$lang->file->clearAllInvalid    = '清空全部';
+$lang->file->clearAllInvalid    = '删除全部';
