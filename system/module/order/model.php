d8a672ebcfff5e48aa9db7e6dc2eecc29789988a
sunjinliang <sunjinliang@cnezsoft.com>
2016-12-07 18:34:30 +0800

    * Adjust processStatus of order

--- system/module/order/model.php
+++ system/module/order/model.php
@@ -394,9 +394,14 @@ class orderModel extends model
     {
         if($order->status == 'finished') return $this->lang->order->statusList['finished'];
         if($order->status == 'canceled') return $this->lang->order->statusList['canceled'];
-        if($order->status == 'expired')  return $this->lang->order->statusList['expired'];
     
-        return $this->lang->order->statusList[$order->deliveryStatus];
+        if($order->payment == 'COD') return $this->lang->order->statusList[$order->deliveryStatus];
+
+        if($order->payment != 'COD')
+        {
+            if($order->payStatus == 'paid') return $this->lang->order->statusList[$order->deliveryStatus];
+            return $this->lang->order->statusList[$order->payStatus];
+        }
     }
 
     /**
