7f12d2f64381af59be7dc0f211b270fae0927bd3
sunjinliang <sunjinliang@cnezsoft.com>
2016-12-13 09:42:52 +0800

    * Adjust the style of lastest thread

--- system/module/widget/view/latestthread.html.php
+++ system/module/widget/view/latestthread.html.php
@@ -11,14 +11,12 @@ $threads = $this->thread->getListForWidget($limit);
     </td>
     <?php 
       $realName = $this->loadModel("user")->getPairs();
-      echo "<td class='w-55px' title='{$realName[$thread->author]}'>";
+      echo "<td class='w-55px text-center' title='{$realName[$thread->author]}'>";
       echo $realName[$thread->author];
       echo "</td>";
     ?>
     <?php if($this->config->forum->postReview == 'open'):?>
     <td class='w-50px text-center'><?php echo zget($lang->thread->statusList, $thread->status);?></td>
-    <?php else:?>
-    <td class='w-50px text-center'><?php echo $thread->replies == 0 ? $lang->thread->unreplied : '';?></td>
     <?php endif;?>
     <td class='w-80px'><?php echo formatTime($thread->addedDate, 'm-d H:i');?></td>
   </tr>
