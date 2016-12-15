a02adf4beced4d9607d812a91e7df3b773e25437
sunjinliang <sunjinliang@cnezsoft.com>
2016-12-15 15:57:49 +0800

    * Fix bug of add the product to cart in mobile

--- www/template/mobile/product/js/view.js
+++ www/template/mobile/product/js/view.js
@@ -22,14 +22,14 @@ $(function()
 {
     $(document).on('click', '.btn-buy', function()
     {
-        window.location.href = $(this).data('url').replace('count', $('#count').val());
+        window.location.href = $(this).data('url').replace('productcount', $('#count').val());
     }).on('click', '.btn-cart', function()
     {
         var $btn = $(this);
         if($btn.hasClass('disabled')) return;
 
         $btn.addClass('disabled')
-        $.getJSON($btn.data('url').replace('count', $('#count').val()), function(data, status)
+        $.getJSON($btn.data('url').replace('productcount', $('#count').val()), function(data, status)
         {
             if(status === 'success')
             {
