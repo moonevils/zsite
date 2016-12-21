aaef58193e14fb5197de1e897a79368c1de98797
guanxiying <guanxiying@xirnagit.com>
2016-12-21 15:05:26 +0800

    * Add mode chose page.

--- system/module/ui/view/browsetheme.html.php
+++ system/module/ui/view/browsetheme.html.php
@@ -1,6 +1,6 @@
 <div id="mainArea" class='row'>
   <?php foreach($themes as $theme):?>
-  <?php $link      = inlink('installtheme', "package={$theme->name}&downLink=&md5=&type=theme");?>
+  <?php $link      = inlink('installtheme', "package={$theme->name}&downLink=&md5=");?>
   <?php $installed = (isset($installedThemes['default'][$theme->name]) or isset($installedThemes['mobile'][$theme->name]));?>
   <div class='col-md-3'>
     <div class='panel theme-panel'>
@@ -13,7 +13,7 @@
           <i class='span-time'><?php echo "<i class='icon icon-time'> </i>" . $theme->time;?></i>
           <i class='span-size'><?php echo "<i class='icon icon-file'> </i>" . helper::formatKB($theme->size / 1024);?></i>
         </div>
-        <?php echo html::a($link, $lang->ui->installTheme, "class='btn btn-primary btn-xs btn-install ' data-toggle='modal'");?>
+        <?php echo html::a($link, $lang->ui->installTheme, "class='btn btn-primary btn-xs btn-install'");?>
       </div>
     </div>
   </div>
@@ -27,3 +27,12 @@
 .btn-install{position:absolute; right: 30px; top:50px;}
 .div-tip{padding-left:10px}
 </style>
+<script>
+$().ready(function()
+{
+    $('.btn-install').click(function()
+    {
+
+    });
+});
+</script>
