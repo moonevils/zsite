7c19ffa5505a718bf6ad5057b8407289ecd7b42e
guanxiying <guanxiying@xirnagit.com>
2016-12-21 14:34:49 +0800

    * Show installed label.

--- system/module/ui/view/browsetheme.html.php
+++ system/module/ui/view/browsetheme.html.php
@@ -1,15 +1,19 @@
 <div id="mainArea" class='row'>
   <?php foreach($themes as $theme):?>
-  <?php $link = inlink('installtheme', "package={$theme->name}&downLink=&md5=&type=theme");?>
+  <?php $link      = inlink('installtheme', "package={$theme->name}&downLink=&md5=&type=theme");?>
+  <?php $installed = (isset($installedThemes['default'][$theme->name]) or isset($installedThemes['mobile'][$theme->name]));?>
   <div class='col-md-3'>
     <div class='panel theme-panel'>
-      <div class='panel-body text-info'>
-        <h3><?php echo $theme->name . ".zip";?></h3>
+      <div class='panel-body'>
+        <div class='theme-title'>
+          <?php echo $theme->name . ".zip";?>
+          <?php if($installed) echo "<i class='label label-success icon icon-check'>{$lang->ui->installed}</i>";?>
+        </div>
         <div class='text-muted'>
           <i class='span-time'><?php echo "<i class='icon icon-time'> </i>" . $theme->time;?></i>
           <i class='span-size'><?php echo "<i class='icon icon-file'> </i>" . helper::formatKB($theme->size / 1024);?></i>
         </div>
-        <?php echo html::a($link, $lang->ui->installTheme, "class='btn btn-success btn-xs btn-install ' data-toggle='modal'");?>
+        <?php echo html::a($link, $lang->ui->installTheme, "class='btn btn-primary btn-xs btn-install ' data-toggle='modal'");?>
       </div>
     </div>
   </div>
@@ -18,6 +22,7 @@
 </div>
 <style>
 .theme-panel > .panel-body{padding-top:4px !important; cursor:pointer;}
+.theme-panel > .panel-body > .theme-title{font-size:16px; padding: 10px 0; color:#555; font-weight:bold;}
 .theme-panel .span-size{margin-left:20px;}
 .btn-install{position:absolute; right: 30px; top:50px;}
 .div-tip{padding-left:10px}
