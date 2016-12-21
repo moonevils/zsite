75fec633c9c2ccbfaf587e0f1b4114936c7d0f5d
sunjinliang <sunjinliang@cnezsoft.com>
2016-12-21 10:18:14 +0800

    * Adjust lang

--- system/module/admin/lang/zh-tw.php
+++ system/module/admin/lang/zh-tw.php
@@ -26,3 +26,22 @@ $lang->admin->reply          = '您今天有 %s 條回覆待審核';
 $lang->admin->comment        = '您今天有 %s 條評論待審核';
 $lang->admin->threadReply    = '您今天有 %s 篇回帖';
 $lang->admin->submittion     = '您今天有 %s 篇投稿待審核';
+
+$lang->admin->registerInfo = "站點已經綁定到蟬知賬號%s，%s";
+$lang->admin->registerPage = '登記頁面';
+$lang->admin->rebind       = "重新綁定";
+
+$lang->js->confirmRebind = "確認要重新綁定蟬知賬號？";
+
+$lang->admin->register = new stdclass();
+$lang->admin->register->common     = '蟬知社區綁定頁面';
+$lang->admin->register->caption    = '蟬知社區登記';
+$lang->admin->register->lblAccount = '請設置您的用戶名，英文字母和數字的組合，三位以上。';
+$lang->admin->register->lblPasswd  = '請設置您的密碼。數字和字母的組合，六位以上。';
+$lang->admin->register->submit     = '登記';
+$lang->admin->register->success    = "登記賬戶成功";
+
+$lang->admin->bind = new stdclass();
+$lang->admin->bind->caption = '已有蟬知官網賬號';
+$lang->admin->bind->submit  = '綁定賬號';
+$lang->admin->bind->success = "關聯賬戶成功";
