14f5b76da0c71f5e7c2da3b51beb74ee9229bf41
sunjinliang <sunjinliang@cnezsoft.com>
2016-12-27 17:11:03 +0800

    * Add index of addedDate of eps_blacklist

--- system/db/upgrade5.5.sql
+++ system/db/upgrade5.5.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `eps_file` CHANGE `size` `size` int(10) UNSIGNED NOT NULL;
 ALTER TABLE `eps_blacklist` ADD `addedDate` datetime NOT NULL AFTER `expiredDate`;
+ALTER TABLE `eps_blacklist` ADD index `addedDate` (`addedDate`);
