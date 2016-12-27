5b1d7b61d249eb58b5b4eda052b76052c8d9eab9
sunjinliang <sunjinliang@cnezsoft.com>
2016-12-27 16:59:46 +0800

    * Merge the getViewsInfo and getViewsList methods

--- system/framework/control.class.php
+++ system/framework/control.class.php
@@ -434,7 +434,8 @@ class control extends baseControl
 
             if(in_array($moduleName . '_' . $methodName, $this->config->replaceViewsPages))
             {
-                $views        = commonModel::getViewsInfo($moduleName, $methodName);
+                $viewsID      = commonModel::parseItemID($moduleName, $methodName);
+                $views        = commonModel::getViews($moduleName, $methodName, $viewsID);
                 $this->output = str_replace($this->config->viewsPlaceholder, $views, $this->output);
             }
             
@@ -443,7 +444,7 @@ class control extends baseControl
                 $beginPos    = strpos($this->output, $this->config->idListPlaceHolder) + strlen($this->config->idListPlaceHolder);
                 $length      = strrpos($this->output, $this->config->idListPlaceHolder) - $beginPos; 
                 $viewsIDList = explode(',', trim(substr($this->output, $beginPos, $length), ',')); 
-                $viewsList   = commonModel::getViewsList($moduleName, $methodName, $viewsIDList);
+                $viewsList   = commonModel::getViews($moduleName, $methodName, $viewsIDList);
                 foreach($viewsList as $viewID => $views)
                 {
                     $this->output = str_replace($this->config->viewsPlaceholder . $viewID . $this->config->viewsPlaceholder, $views, $this->output);
