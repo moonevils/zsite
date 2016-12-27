9e5261fc3a5b1bf55225b5b8cd795190663febe8
tengfei <tengfei@cnezsoft.com>
2016-12-27 14:13:11 +0800

    *adjust tags to keywords

--- system/module/admin/lang/en.php
+++ system/module/admin/lang/en.php
@@ -27,21 +27,21 @@ $lang->admin->comment        = 'Today you have %s comments to review.';
 $lang->admin->threadReply    = 'Today you have %s replies.';
 $lang->admin->submittion     = 'Today you have %s submissions to review.';
 
-$lang->admin->registerInfo = "The site has binded the account with chanzhi%s, %s";
+$lang->admin->registerInfo = "The site has binded the account with Changer %s, %s";
 $lang->admin->registerPage = 'Register Page';
 $lang->admin->rebind       = "Rebind";
 
-$lang->js->confirmRebind = "Are you sure to rebind the account of chanzhi?";
+$lang->js->confirmRebind = "Are you sure to rebind the account of Changer?";
 
 $lang->admin->register = new stdclass();
-$lang->admin->register->common     = 'The page registering account of chanzhi';
-$lang->admin->register->caption    = 'Register chanzhi account';
+$lang->admin->register->common     = 'The page registering account of Changer';
+$lang->admin->register->caption    = 'Register Changer account';
 $lang->admin->register->lblAccount = 'Please set your nickname which has to be combination of letters and numbers. The nickname should be more than three characters.';
 $lang->admin->register->lblPasswd  = 'Please set your password which has to be combination of letters and numbers. The password should be more than six characters.';
 $lang->admin->register->submit     = 'Submit';
 $lang->admin->register->success    = "Submit success";
 
 $lang->admin->bind = new stdclass();
-$lang->admin->bind->caption = 'The account of chanzhi';
+$lang->admin->bind->caption = 'The account of Changer';
 $lang->admin->bind->submit  = 'Bind account';
 $lang->admin->bind->success = "Bind account successfully";
