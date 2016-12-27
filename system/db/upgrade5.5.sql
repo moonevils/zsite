67a7b47c8c2bf360cb35219e9df5505d1dae6fa9
guanxiying <guanxiying@xirnagit.com>
2016-12-27 15:50:14 +0800

    * Add effectID.

--- system/db/upgrade5.5.sql
+++ system/db/upgrade5.5.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `eps_file` CHANGE `size` `size` int(10) UNSIGNED NOT NULL;
 ALTER TABLE `eps_blacklist` ADD `addedDate` datetime NOT NULL AFTER `expiredDate`;
+ALTER TABLE `eps_block` ADD effectID mediumint(8) UNSIGNED NOT NULL default 0;
