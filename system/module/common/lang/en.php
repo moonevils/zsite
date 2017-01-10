5a8e810a2dcdec66fdd4e0fa21dd928836ee0238
sunjinliang <sunjinliang@cnezsoft.com>
2017-01-10 16:21:19 +0800

    * Adjust lang of en

--- system/module/common/lang/en.php
+++ system/module/common/lang/en.php
@@ -42,7 +42,7 @@ $lang->poweredByAdmin = "<span id='poweredBy'> is powered by <a href='http://www
 $lang->newVersion     = "Note: Changer CMS has released <span id='version'></span> on <span id='releaseDate'></span>. <a href='' target='_blank' id='upgradeLink'>Download it NOW!</a>";
 $lang->execInfo       = "<span id='execInfoBar' class='hide'><span class='text-left'>SQL query：<b>%s</b> <br> Memory footprint: <b>%s</b><br> PHP E-time: <b>%s</b> s</span></span>";
 $lang->customCssError = "Fail to load the self-defined css file, if you are the administrator of this site, please reset the apperence of the site in the admin";
-$lang->redirecting    = "Redirecting to %s，<a href='%s'>Redirect immediately</a>";
+$lang->redirecting    = "<span class='text-muted'>Redirecting......</span> <a class='btn-redirec' href='%s'><i class='icon icon-hand-right'></i>Redirect</a>";
 
 /* Global lang items. */
 $lang->home             = 'Home';
