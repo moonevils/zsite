91556040d3690bfb677e906a3fb19325071e0fa9
sunjinliang <sunjinliang@cnezsoft.com>
2017-01-16 14:18:14 +0800

    * Remove the yangcong auth

--- system/module/site/view/setoauth.html.php
+++ system/module/site/view/setoauth.html.php
@@ -12,44 +12,6 @@
 ?>
 <?php include '../../common/view/header.admin.html.php';?>
 <div class='row'>
-  <div class='col-sm-6 panel-box yangcong-panel pull-right'>
-    <div class='panel'>
-      <div class='panel-heading'>
-        <strong><?php echo $lang->site->setYangcong?></strong>
-        <div class='panel-actions'>
-          <?php echo html::a('http://api.chanzhi.org/goto.php?item=help_yangcongoauth', '<i class="icon-question-sign"></i> ' . $lang->site->oauthHelp, "target='_blank' class='btn btn-link'");?>
-        </div>
-      </div>
-      <div class='panel-body'>
-      <form method='post' id='ajaxForm' action='<?php echo inlink('setyangcong')?>' class='form-horizontal'>
-          <table class="table table-form">
-            <tr>
-              <th class='w-100px'><?php echo $lang->site->yangcong->appID;?></th>
-              <td class='w-300px'>
-                <?php echo html::input('appID', zget($setting, 'appID', ' '), "class='form-control'");?>
-              </td>
-              <td></td>
-            </tr>
-            <tr>
-              <th><?php echo $lang->site->yangcong->key;?></th>
-              <td>
-                <?php echo html::input('key', zget($setting, 'key', ' '), "class='form-control'");?>
-              </td>
-            </tr>
-            <tr>
-              <th><?php echo $lang->site->yangcong->auth;?></th>
-              <td>
-                <?php echo html::input('auth', zget($setting, 'auth', ' '), "class='form-control'");?>
-              </td>
-            </tr>
-            <tr>
-             <th></th> <td><?php echo html::submitButton();?></td>
-            </tr>
-          </table>
-        </form>
-      </div>
-    </div>
-  </div>
 <?php foreach($lang->user->oauth->providers as $providerCode => $providerName):?>
 <?php isset($this->config->oauth->$providerCode) ? $oauth = json_decode($this->config->oauth->$providerCode) : $oauth = '';?>
 <div class='col-sm-6 panel-box <?php echo $providerCode . '-panel';?>'>
