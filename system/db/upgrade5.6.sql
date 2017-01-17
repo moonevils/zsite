a7fd6d7e3b031706855c6b523e622b0ee3c74bf3
sunjinliang <sunjinliang@cnezsoft.com>
2017-01-17 14:12:00 +0800

    * Adjust the upgrade sql

---
+++ system/db/upgrade5.6.sql
@@ -0,0 +1,11 @@
+ALTER table `eps_article`  CHANGE `submittion` `submission` enum('0', '1', '2', '3') NOT NULL DEFAULT '0';
+UPDATE `eps_article`    SET `type` = 'submission' WHERE `type` = 'submittion';
+UPDATE `eps_config`     SET `value` = replace(value, 'submittion', 'submission') WHERE `key` = 'modules';
+UPDATE `eps_config`     SET `value` = replace(value, 'submittion', 'submission') WHERE `key` = 'home';
+UPDATE `eps_config`     SET `method` = 'approvesubmission' WHERE `method` = 'approveSubmittion';
+UPDATE `eps_file`       SET `objectType` = 'submission' WHERE `objectType` = 'submittion';
+UPDATE `eps_score`      SET `method` = 'approvesubmission' WHERE `method` = 'approvesubmittion';
+UPDATE `eps_score`      SET `method` = 'approvesubmission' WHERE `method` = 'approveSubmittion';
+UPDATE `eps_statlog`    SET `url` = replace(url, 'submittion', 'submission');
+UPDATE `eps_statreport` SET `item` = replace(item, 'submittion', 'submission');
+UPDATE `eps_widget`     SET `type` = 'submission' WHERE `type` = 'submittion';
