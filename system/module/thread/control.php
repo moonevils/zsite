bb0330fd06657af80a7d9253a8e719ef908922ec
sunjinliang <sunjinliang@cnezsoft.com>
2017-01-17 15:59:01 +0800

    * Add the redirect to the newest reply of thread

--- system/module/thread/control.php
+++ system/module/thread/control.php
@@ -137,6 +137,7 @@ class thread extends control
      */
     public function view($threadID, $pageID = 1)
     {
+        if(strpos($pageID, '?') !== false) $pageID = explode('?', $pageID)[0];
         $this->loadModel('guarder');
         $thread = $this->thread->getByID($threadID);
         if(!$thread or $thread->hidden) die(js::locate('back'));
