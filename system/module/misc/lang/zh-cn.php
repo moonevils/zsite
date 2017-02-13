01875bd6dbb36ff3005af779a297449a365a49a2
sunjinliang <sunjinliang@cnezsoft.com>
2017-02-13 08:24:35 +0800

    * Remove the auto upgrade from frame

--- system/module/misc/lang/zh-cn.php
+++ system/module/misc/lang/zh-cn.php
@@ -9,20 +9,3 @@ $lang->misc->forum       = '论坛交流';
 $lang->misc->thanksTitle      = '蝉知构建于众多优秀的开源项目之上';
 $lang->misc->thanksFooter     = '感谢%s提供安全审计';
 $lang->misc->thanksObjectName = '站长安全网';
-
-$lang->misc->currentVersion   = '当前版本 ：';
-$lang->misc->isLatestVersion  = '当前蝉知版本是最新版，无需升级';
-$lang->misc->autoUpgradeTip   = '最新版本为 ：';   
-$lang->misc->startUpgrade     = '开始升级';   
-$lang->misc->upgrade          = '升级';   
-$lang->misc->checkUpgradeInfo = '查看升级内容';
-
-$lang->misc->downloadingPackage = '正在下载更新包';
-$lang->misc->downloadedPackage  = '更新包下载完成';
-$lang->misc->checkingPackage    = '正在验证更新包';
-$lang->misc->checkedPackage     = '更新包验证完成';
-$lang->misc->extractingPackage  = '解压缩更新包';
-$lang->misc->downloadingpackage = '准备升级中';
-
-$lang->misc->invalidUpgradePath['no_dir']       = '请创建/tmp/upgradepath';
-$lang->misc->invalidUpgradePath['not_writable'] = '请修改权限/tmp/upgradepath';
