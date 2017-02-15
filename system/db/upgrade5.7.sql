5a0e3bcbbf8c270f0f8253d0111cda6bfd62fd46
sunjinliang <sunjinliang@cnezsoft.com>
2017-02-15 14:08:52 +0800

    * Change the width of key of eps_config

--- system/db/upgrade5.7.sql
+++ system/db/upgrade5.7.sql
@@ -4,3 +4,4 @@ ALTER table `eps_statlog`  CHANGE `ip` `ip` char(40) NOT NULL;
 ALTER table `eps_thread`   CHANGE `ip` `ip` char(40) NOT NULL;
 ALTER table `eps_user`     CHANGE `ip` `ip` char(40) NOT NULL DEFAULT '';
 ALTER table `eps_log`      CHANGE `ip` `ip` char(40) NOT NULL;
+ALTER table `eps_config`   CHANGE `key` `key` char(50) DEFAULT NULL;
