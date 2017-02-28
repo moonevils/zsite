25e0f06df3dd4f2771b1f3a5e28e77b897b8eef9
guanxiying <guanxiying@xirnagit.com>
2017-02-28 15:32:17 +0800

    * Fix createpaylink bug.

--- system/module/order/model.php
+++ system/module/order/model.php
@@ -203,7 +203,7 @@ class orderModel extends model
      */
     public function createPayLink($order, $type = '')
     {
-        if(is_callable(array($this, "create{$order->payment}PayLink"))) return call_user_func(array($this,"create{$order->payment}PayLink"), $order);
+        if($order->payment and is_callable(array($this, "create{$order->payment}PayLink"))) return call_user_func(array($this,"create{$order->payment}PayLink"), $order);
         return helper::createLink('order', 'check', "orderID=$order->id");
     }
 
