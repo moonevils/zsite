baf629accaa17f6590612ee900836fc8fa15eb14
daitingting <daitingting@xirangit.com>
2017-03-21 14:56:14 +0800

    * Fix a bug when .books > .active does not exist.

--- www/template/default/book/js/read.js
+++ www/template/default/book/js/read.js
@@ -8,21 +8,24 @@ $(document).ready(function()
     });  
     //scroll
     function yrScroll(){
-        if( $("#book").offset() ){
+        if( $("#book").offset()){
             var headerHeight = $("#book").offset().top;
         }
         var listTitleHeight = $(".book-catalog .panel-heading").height();
         //init
-        $(".book-catalog").css({
-            "max-height":"600px",
-            "overflow":"auto"
-        });
-        var listScrollTop =  $(".books .active").position().top;
-        var listMoveSize = listScrollTop > ( $(".bookScrollListsBox").height() - listTitleHeight ) / 2 ? listScrollTop : 0;
-        var scrollMoveSize = listMoveSize / $(".books").height(); 
-        $(".bookScrollListsBox").scrollTop(
-            $(".bookScrollListsBox .books").height() * scrollMoveSize -($(".bookScrollListsBox").height() / 2 - $(".bookScrollListsBox .panel-heading").height() - 47)
-        );
+        $(".book-catalog").css({"max-height": "600px","overflow":"auto"});
+
+        if($('.books .active').length)
+        {
+            var listScrollTop =  $(".books .active").position().top;
+            var listMoveSize = listScrollTop > ( $(".bookScrollListsBox").height() - listTitleHeight ) / 2 ? listScrollTop : 0;
+            var scrollMoveSize = listMoveSize / $(".books").height(); 
+            $(".bookScrollListsBox").scrollTop
+            (
+                $(".bookScrollListsBox .books").height() * scrollMoveSize -($(".bookScrollListsBox").height() / 2 - $(".bookScrollListsBox .panel-heading").height() - 47)
+            );
+        }
+
         //scroll event
         $(document).on("scroll", function (){
              //minHeight
