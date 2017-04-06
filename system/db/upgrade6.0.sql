7843233fc4252fdcdfba4ad29c8b685f423e76fd
daitingting <daitingting@xirangit.com>
2017-04-06 09:30:07 +0800

    * Set order.

--- system/db/upgrade6.0.sql
+++ system/db/upgrade6.0.sql
@@ -1,12 +1,15 @@
-ALTER TABLE `eps_article` ADD `titleColor`     varchar(10) NOT NULL AFTER `onlyBody`;
-ALTER TABLE `eps_product` ADD `titleColor`     varchar(10) NOT NULL AFTER `js`;
-ALTER TABLE `eps_user`    ADD `notification`   varchar(20) NOT NULL DEFAULT 'mail,' AFTER `security`;
-ALTER TABLE `eps_file`    CHANGE `primary` `order` smallint(5) unsigned NOT NULL;
+ALTER TABLE `eps_article`  ADD `titleColor`     varchar(10) NOT NULL AFTER `onlyBody`;
+ALTER TABLE `eps_product`  ADD `titleColor`     varchar(10) NOT NULL AFTER `js`;
+ALTER TABLE `eps_user`     ADD `notification`   varchar(20) NOT NULL DEFAULT 'mail,' AFTER `security`;
 ALTER TABLE `eps_category` ADD `discussion` enum('0', '1') NOT NULL DEFAULT '0' AFTER `unsaleable`;
-ALTER TABLE `eps_thread` ADD `discussion` enum('0', '1') NOT NULL DEFAULT '0' AFTER `title`;
-ALTER TABLE `eps_reply` ADD `reply` mediumint(8) unsigned NOT NULL AFTER `thread`;
-ALTER TABLE `eps_order` ADD `refundSN` char(50) NOT NULL AFTER `sn`;
+ALTER TABLE `eps_thread`   ADD `discussion` enum('0', '1') NOT NULL DEFAULT '0' AFTER `title`;
+ALTER TABLE `eps_reply`    ADD `reply` mediumint(8) unsigned NOT NULL AFTER `thread`;
+ALTER TABLE `eps_order`    ADD `refundSN` char(50) NOT NULL AFTER `sn`;
+
 ALTER TABLE `eps_order` CHANGE `payStatus` `payStatus` enum('not_paid', 'paid', 'refunded') NOT NULL DEFAULT 'not_paid';
+ALTER TABLE `eps_file`  CHANGE `primary` `order` smallint(5) unsigned NOT NULL;
+
+UPDATE `eps_file` SET `order` = 2 WHERE `order` = 0;
 
 -- DROP TABLE IF EXISTS `eps_action`;
 CREATE TABLE IF NOT EXISTS `eps_action` (
