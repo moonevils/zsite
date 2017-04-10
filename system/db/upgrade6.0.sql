9b1607536178673ae2f43190d415f76a2ba7723d
daitingting <daitingting@xirangit.com>
2017-04-10 19:11:10 +0800

    * Adjust field.

--- system/db/upgrade6.0.sql
+++ system/db/upgrade6.0.sql
@@ -1,6 +1,6 @@
-ALTER TABLE `eps_article`  ADD `titleColor`     varchar(10) NOT NULL AFTER `onlyBody`;
-ALTER TABLE `eps_product`  ADD `titleColor`     varchar(10) NOT NULL AFTER `js`;
-ALTER TABLE `eps_user`     ADD `notification`   varchar(20) NOT NULL DEFAULT 'mail,' AFTER `security`;
+ALTER TABLE `eps_article`  ADD `titleColor`     varchar(10) NOT NULL AFTER `title`;
+ALTER TABLE `eps_product`  ADD `titleColor`     varchar(10) NOT NULL AFTER `name`;
+ALTER TABLE `eps_user`     ADD `notification`   varchar(20) NOT NULL DEFAULT '' AFTER `security`;
 ALTER TABLE `eps_category` ADD `discussion` enum('0', '1') NOT NULL DEFAULT '0' AFTER `unsaleable`;
 ALTER TABLE `eps_thread`   ADD `discussion` enum('0', '1') NOT NULL DEFAULT '0' AFTER `title`;
 ALTER TABLE `eps_reply`    ADD `reply` mediumint(8) unsigned NOT NULL AFTER `thread`;
