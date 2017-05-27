6e56062ba781cfeded71d9832cc64e6932a3a0f8
daitingting <daitingting@xirangit.com>
2017-05-27 09:27:51 +0800

    * Adjust upgrade version.

--- system/db/upgrade6.2.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `eps_book` ADD `link` varchar(255) NOT NULL AFTER `order`;
-ALTER TABLE `eps_book` ADD `articleID` smallint(5) unsigned not null default 0 AFTER `id`;
-ALTER TABLE `eps_wx_public` ADD `remindUsers` text NOT NULL AFTER `addedDate`;
