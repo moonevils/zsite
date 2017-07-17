fa11c88b339956cb48e090783d739aec69fb3bea
daitingting <daitingting@xirangit.com>
2017-07-17 15:49:39 +0800

    * Adjust positon for login page.

--- www/template/default/user/login.html.php
+++ www/template/default/user/login.html.php
@@ -19,12 +19,14 @@ js::set('random', $this->session->random);
             <div class='form-group hiding'><div id='formError' class='alert alert-danger'></div></div>
             <div class='form-group'><?php echo html::input('account','',"placeholder='{$lang->user->inputAccountOrEmail}' class='form-control input-lg'");?></div>
             <div class='form-group'><?php echo html::password('password','',"placeholder='{$lang->user->inputPassword}' class='form-control input-lg'");?></div>
-            <?php if($config->mail->turnon and $this->config->site->resetPassword == 'open') echo html::a(inlink('resetpassword'), $lang->user->recoverPassword, "id='reset-pass'");?>
-            <?php if(!$formOnly) echo html::a(inlink('register'), $lang->user->register->instant, "id='register'");?>
             <?php echo html::submitButton($lang->user->login->common, 'btn btn-primary btn-wider btn-lg btn-block');?> &nbsp; &nbsp; 
             <?php echo html::hidden('referer', $referer);?>
           </form>
           <?php include TPL_ROOT . 'user/oauthlogin.html.php';?>
+          <span class='regAndReset pull-right'>
+            <?php if($config->mail->turnon and $this->config->site->resetPassword == 'open') echo html::a(inlink('resetpassword'), $lang->user->recoverPassword, "id='reset-pass'");?>
+            <?php if(!$formOnly) echo html::a(inlink('register'), $lang->user->register->instant, "id='register'");?>
+          </span>
         </div>
       </div>
     </div>
