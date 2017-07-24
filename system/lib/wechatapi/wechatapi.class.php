f93f02617956ef719febd52a18208fc19447afe9
daitingting <daitingting@xirangit.com>
2017-07-24 17:40:51 +0800

    * Remove HTTP_RAW_POST_DATA.

--- system/lib/wechatapi/wechatapi.class.php
+++ system/lib/wechatapi/wechatapi.class.php
@@ -201,7 +201,7 @@ class wechatapi
     {
         $this->rawData = '';
         $this->message = new stdclass();
-        $post = isset($GLOBALS["HTTP_RAW_POST_DATA"]) ? $GLOBALS["HTTP_RAW_POST_DATA"] : file_get_contents('php://input');
+        $post = file_get_contents('php://input');
         if($post)
         {
             $this->rawData = str_replace('&', '&amp;', $post);
