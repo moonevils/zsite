f06e62cf0293e272fab0b6782332be2cba150060
daitingting <daitingting@xirangit.com>
2017-08-18 10:40:47 +0800

    * Adjust style for setemail page.

--- www/template/default/user/setemail.html.php
+++ www/template/default/user/setemail.html.php
@@ -8,28 +8,25 @@
         <div class='panel-heading'><strong><i class='icon-edit'></i> <?php echo $lang->user->setEmail;?></strong></div>
         <div class='panel-body'>
           <form method='post' id='ajaxForm' class='form form-horizontal' data-checkfingerprint='1'>
-            <div class='form-group'>
-              <label for='oldPwd' class='col-md-2 col-sm-3 control-label'><?php echo $lang->user->password;?></label>
-              <div class='col-md-6 col-sm-6'>
-                <?php echo html::password('oldPwd', '', "class='form-control' placeholder='{$lang->user->placeholder->password}'");?>
-              </div>
-            </div>
-            <div class='form-group'>
-              <label for='email' class='col-md-2 col-sm-3 control-label'><?php echo $lang->user->newEmail;?></label>
-              <div class='col-md-6 col-sm-6'>
-                <?php echo html::input('email', '', "class='form-control'");?>
-              </div>
-            </div>
-            <div class='form-group'>
-              <label for='captcha' class='col-md-2 col-sm-3 control-label'><?php echo $lang->user->captcha;?></label>
-              <div class='col-md-6 col-sm-6'>
-                <?php echo html::input('captcha', '', "class='form-control' placeholder='{$lang->user->placeholder->verifyCode}'");?>
-              </div>
-                <?php echo html::a($this->createLink('mail', 'sendmailcode'), $lang->user->getEmailCode, "id='mailSender' class='btn btn-sm btn-primary'");?>
-            </div>
-            <div class='form-group'>
-              <div class='col-md-6 col-sm-6 col-md-offset-2 col-sm-offset-3'><?php echo html::submitButton() . html::hidden('token', $token);?></div>
-            </div>
+            <table class='table talbe-form table-borderless'>
+              <tr>
+                <th class='text-right w-80px'><?php echo $lang->user->password;?></th>
+                <td class='w-p50'><?php echo html::password('oldPwd', '', "class='form-control' placeholder='{$lang->user->placeholder->password}'");?></td><td></td>
+              </tr>
+              <tr>
+                <th class='text-right'><?php echo $lang->user->newEmail;?></th>
+                <td><?php echo html::input('email', '', "class='form-control'");?></td><td></td>
+              </tr>
+              <tr>
+                <th class='text-right'><?php echo $lang->user->captcha;?></th>
+                <td><?php echo html::input('captcha', '', "class='form-control' placeholder='{$lang->user->placeholder->verifyCode}'");?></td>
+                <td class='text-middle'><?php echo html::a($this->createLink('mail', 'sendmailcode'), $lang->user->getEmailCode, "id='mailSender' class='btn btn-sm btn-primary'");?></td>
+              </tr>
+              <tr>
+                <th></th>
+                <td colspan='2'><?php echo html::submitButton() . html::hidden('token', $token);?></td>
+              </tr>
+            </table>
           </form>
         </div>
       </div>
