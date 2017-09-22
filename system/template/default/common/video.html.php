f13e8f8d2d00a9e8a180eea0d16e03736c23beb4
Xiying Guan <guanxiying@cnezsoft.com>
2017-09-22 09:52:35 +0800

    * Add check videojs function.

--- system/template/default/common/video.html.php
+++ system/template/default/common/video.html.php
@@ -7,8 +7,7 @@ $videoHtml = <<<EOT
 <video id="VIDEO_ID"
 class="video-js vjs-default-skin vjs-big-play-centered "
 controls preload="auto" loop='loop'
-width="VIDEO_WIDTH" height="VIDEO_HEIGHT"
-poster="http://www.eps.com/data/poster.jpg" >
+width="VIDEO_WIDTH" height="VIDEO_HEIGHT">
 <source src="VIDEO_SRC" />
 </video>
 EOT;
@@ -17,20 +16,24 @@ EOT;
 $(function()
 {
     var videoContainer = <?php echo json_encode($videoHtml);?>;
-    $('embed').hide().each(function(index)
+    $('embed').each(function(index)
     {
-        var $embed = $(this),
-            src    = $embed.attr('src'),
-            w      = $embed.width(),
-            h      = $embed.height(),
-            containerID = 'video_' + index;
-        $container = videoContainer.replace(/VIDEO_SRC/, src);
-        $container = $container.replace(/VIDEO_WIDTH/, w);
-        $container = $container.replace(/VIDEO_HEIGHT/, h);
-        $container = $container.replace(/VIDEO_ID/, containerID);
-        $(this).replaceWith($container);
-        $('#'+containerID).width(w);
-        $('#'+containerID).height(h);
+        if($(this).hasClass('videojs')) 
+        {
+            var $embed = $(this),
+                src    = $embed.attr('src'),
+                w      = $embed.width(),
+                h      = $embed.height(),
+                containerID = 'video_' + index;
+
+            $container = videoContainer.replace(/VIDEO_SRC/, src);
+            $container = $container.replace(/VIDEO_WIDTH/, w);
+            $container = $container.replace(/VIDEO_HEIGHT/, h);
+            $container = $container.replace(/VIDEO_ID/, containerID);
+            $(this).replaceWith($container);
+            $('#'+containerID).width(w);
+            $('#'+containerID).height(h);
+        }
     })
 });
 </script>
