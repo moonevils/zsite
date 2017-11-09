04d3732c74ef49c4e9e75fccda43d04ab92a6e4c
daitingting <daitingting@xirangit.com>
2017-11-09 13:26:01 +0800

    * Use autoplay instead of autostart.

--- system/module/upgrade/lang/zh-tw.php
+++ system/module/upgrade/lang/zh-tw.php
@@ -101,3 +101,4 @@ $lang->upgrade->fromVersions['6_3_beta'] = '6.3.beta';
 $lang->upgrade->fromVersions['6_4']      = '6.4';
 $lang->upgrade->fromVersions['6_4_1']    = '6.4.1';
 $lang->upgrade->fromVersions['6_5']      = '6.5';
+$lang->upgrade->fromVersions['6_6']      = '6.6';
