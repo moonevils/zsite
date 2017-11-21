a8b74df11711eae8ceb3b77f352e32a40ba30adb
daitingting <daitingting@xirangit.com>
2017-11-21 14:47:37 +0800

    * Adjust style.

--- system/template/default/block/latestproduct.image.html.php
+++ system/template/default/block/latestproduct.image.html.php
@@ -1,5 +1,7 @@
 <style>
-.panel-body .cards-custom .card > .card-heading {min-height: 20px; height: 20px; padding: 10px; font-size: 13px;}
+.panel-body .cards-custom .card > .card-heading {min-height: 40px; height: 40px; padding: 10px; font-size: 13px; position: relative;}
+.panel-body .cards-custom .card > .card-heading > strong {width: 40px; height: 40px; padding: 10px; font-size: 13px;}
+.panel-body .cards-custom .card > .card-heading > .views {position: absolute; right: 0; top: 10px;}
 .panel-body .cards-custom .card > .card-content {padding: 0 10px 10px 10px; margin-bottom: 10px;}
 </style>
 
@@ -47,7 +49,7 @@
           <?php endif;?>
 
           <?php if(isset($content->showViews) and $content->showViews):?>
-          <div class='pull-right'><i class="icon icon-eye-open"></i> <?php echo $product->views;?></div>
+          <div class='views'><i class="icon icon-eye-open"></i> <?php echo $product->views;?></div>
           <?php endif;?>
         </div>
 
