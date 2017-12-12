5788351005c283fb195d4057b6a39c89985fa012
wumo <wumo@cnezsoft.com>
2017-12-12 18:28:10 +0800

    Change the file style to raintpl formate.

--- system/template/mobile/blog/header.html.php
+++ system/template/mobile/blog/header.html.php
@@ -1,24 +1,22 @@
-{if($extView = $control->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
+{if($extView = $control->getExtViewFile(__FILE__))}
+  {include $extView}
+  {@return helper::cd()}
+{/if}
 {include $control->loadModel('ui')->getEffectViewFile('mobile', 'common', 'header.lite')}
 <div class='block-region region-all-top blocks' data-region='all-top'>{$control->block->printRegion($layouts, 'all', 'top')}</div>
 <header class='appbar fix-top' id='appbar'>
   <div class='appbar-title'>
     <a href='{!echo $webRoot}'>
-{*php*}
-      $logoSetting = isset($control->config->site->logo) ? json_decode($control->config->site->logo) : new stdclass();
-      $logo        = false;
-      if(isset($logoSetting->$templateName->themes->all))        $logo = $logoSetting->$templateName->themes->all;
-      if(isset($logoSetting->$templateName->themes->$themeName)) $logo = $logoSetting->$templateName->themes->$themeName; 
-      if($logo)
-      {
-          $logo->extension = $control->loadModel('file')->getExtension($logo->pathname);
-          echo html::image($control->loadModel('file')->printFileURL($logo->pathname, $logo->extension), "class='logo' alt='{$control->config->company->name}' title='{$control->config->company->name}'");
-      }
-{else}
-      {
-          echo '<h4>' . $control->config->site->name . '</h4>';
-      }
-{*/php*}
+     {$logoSetting = isset($control->config->site->logo) ? json_decode($control->config->site->logo) : new stdclass()}
+     {$logo = false}
+     {if(isset($logoSetting->$templateName->themes->all))} {$logo = $logoSetting->$templateName->themes->all} {/if}
+     {if(isset($logoSetting->$templateName->themes->$themeName))} {$logo = $logoSetting->$templateName->themes->$themeName} {/if}
+     {if($logo)}
+       {$logo->extension = $control->loadModel('file')->getExtension($logo->pathname)}
+       {!html::image($control->loadModel('file')->printFileURL($logo->pathname, $logo->extension), "class='logo' alt='{{$control->config->company->name}}' title='{{$control->config->company->name}}'")}
+     {else}
+       {!echo '<h4>' . $control->config->site->name . '</h4>'}
+     {/if}
     </a>
   </div>
   <div class='appbar-actions'>
@@ -26,11 +24,11 @@
     <div class='dropdown'>
       <button type='button' class='btn' data-toggle='dropdown' id='searchToggle'><i class='icon-search'></i></button>
       <div class='dropdown-menu fade search-bar' id='searchbar'>
-        <form action='{!echo helper::createLink('search')?>' method='get' role='search'>
+        <form action='{!helper::createLink('search')}' method='get' role='search'>
           <div class='input-group'>
             {$keywords = ($control->app->getModuleName() == 'search') ? $control->session->serachIngWord : ''}
-            {!echo html::input('words', $keywords, "class='form-control' placeholder=''")}
-            {if($control->config->requestType == 'GET') echo html::hidden($control->config->moduleVar, 'search') . html::hidden($control->config->methodVar, 'index')}
+            {!html::input('words', $keywords, "class='form-control' placeholder=''")}
+            {if($control->config->requestType == 'GET')} {!html::hidden($control->config->moduleVar, 'search') . html::hidden($control->config->methodVar, 'index')} {/if}
             <div class='input-group-btn'>
               <button class='btn default' type='submit'><i class='icon icon-search'></i></button>
             </div>
@@ -41,12 +39,12 @@
     {/if}
     <div class='dropdown'>
       {if(!isset($control->config->site->type) or $control->config->site->type != 'blog')}
-      {!echo html::a($config->webRoot, '<i class="icon-home icon-large"></i>', "class='btn'")}
+        {!html::a($config->webRoot, '<i class="icon-home icon-large"></i>', "class='btn'")}
       {/if}
 
       <button type='button' class='btn' data-toggle='dropdown'><i class='icon-bars'></i></button>
       <ul class='dropdown-menu dropdown-menu-right'>
-        {!echo $control->config->siteNavHolder;}
+        {$control->config->siteNavHolder}
       </ul>
     </div>
   </div>
@@ -59,43 +57,34 @@
     {$subnavs = ''}
     {foreach($navs as $nav1)}
       <li class='{!echo $nav1->class?>'>
-{*php*}
-      if(empty($nav1->children))
+      {if(empty($nav1->children))}
+        {!html::a($nav1->url, $nav1->title, ($nav1->target != 'modal') ? "target='$nav1->target'" : "data-toggle='modal'")}
+      {else}
       {
-          echo html::a($nav1->url, $nav1->title, ($nav1->target != 'modal') ? "target='$nav1->target'" : "data-toggle='modal'");
+        {echo html::a("#sub-{$nav1->class}", $nav1->title . " <i class='icon-caret-down'></i>", ($nav1->target != 'modal') ? "target='$nav1->target'" : "data-toggle='modal'")}
+        {$subnavs .= "<ul class='nav' id='sub-{$nav1->class}'>\n"}
+        {foreach($nav1->children as $nav2)}
+          {$subnavs .= "<li class='{$nav2->class}'>"}
+          {if(empty($nav2->children))}
+            {$subnavs .= html::a($nav2->url, $nav2->title, ($nav2->target != 'modal') ? "target='$nav2->target'" : "data-toggle='modal' class='text-important'")}
+          {else}
+            {$subnavs .= html::a("javascript:;", $nav2->title . " <i class='icon-caret-down'></i>", "data-toggle='dropdown' class='text-important'")}
+            {$subnavs .= "<ul class='dropdown-menu'>"}
+            {foreach($nav2->children as $nav3)}
+              {$subnavs .= "<li>" . html::a($nav3->url, $nav3->title, ($nav3->target != 'modal') ? "target='$nav3->target'" : "data-toggle='modal' class='text-important'") . '</li>'}
+            {/foreach}
+            {$subnavs .= "</ul>\n"}
+          {/if}
+          {$subnavs .= "</li>\n"}
+        {/foreach}
+        {$subnavs .= "</ul>\n"}
       }
-{else}
-      {
-          echo html::a("#sub-{$nav1->class}", $nav1->title . " <i class='icon-caret-down'></i>", ($nav1->target != 'modal') ? "target='$nav1->target'" : "data-toggle='modal'");
-          $subnavs .= "<ul class='nav' id='sub-{$nav1->class}'>\n";
-          foreach($nav1->children as $nav2)
-          {
-              $subnavs .= "<li class='{$nav2->class}'>";
-              if(empty($nav2->children))
-              {
-                  $subnavs .= html::a($nav2->url, $nav2->title, ($nav2->target != 'modal') ? "target='$nav2->target'" : "data-toggle='modal' class='text-important'");
-              }
-{else}
-              {
-                  $subnavs .= html::a("javascript:;", $nav2->title . " <i class='icon-caret-down'></i>", "data-toggle='dropdown' class='text-important'");
-                  $subnavs .= "<ul class='dropdown-menu'>";
-                  foreach($nav2->children as $nav3)
-                  {
-                      $subnavs .= "<li>" . html::a($nav3->url, $nav3->title, ($nav3->target != 'modal') ? "target='$nav3->target'" : "data-toggle='modal' class='text-important'") . '</li>';
-                  }
-                  $subnavs .= "</ul>\n";
-              }
-              $subnavs .= "</li>\n";
-          }
-          $subnavs .= "</ul>\n";
-      }
-{*/php*}
       </li>
     {/foreach}<!-- end nav1 -->
     </ul>
   </div>
   <div class='subnavs fade'>
-    {!echo $subnavs}
+    {$subnavs}
   </div>
 </nav>
 
