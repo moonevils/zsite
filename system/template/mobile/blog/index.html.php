252c6377e5c46e405b5df0a11c4395c69587546e
wumo <wumo@cnezsoft.com>
2017-12-12 18:28:10 +0800

    Change the file style to raintpl formate.

--- system/template/mobile/blog/index.html.php
+++ system/template/mobile/blog/index.html.php
@@ -1,4 +1,4 @@
-{*php*}
+{*php
 /**
  * The blog index view file for mobile template of chanzhiEPS.
  *
@@ -9,76 +9,72 @@
  * @version     $Id$
  * @link        http://www.chanzhi.org
  */
-{*/php*}
+/php*}
 {include $control->loadModel('ui')->getEffectViewFile('mobile', 'blog', 'header')}
-{if(!empty($category->id)) js::set('pageLayout', $control->block->getLayoutScope('blog_index', $category->id))}
+{if(!empty($category->id))} {!js::set('pageLayout', $control->block->getLayoutScope('blog_index', $category->id))}{/if}
 {if(isset($articleIdList))}
-<script>{!echo "place" . md5(time()). "='" . $config->idListPlaceHolder . $articleIdList . $config->idListPlaceHolder . "';"}</script>
+  <script>{!echo "place" . md5(time()). "='" . $config->idListPlaceHolder . $articleIdList . $config->idListPlaceHolder . "';"}</script>
 {else}
-<script>{!echo "place" . md5(time()) . "='" . $config->idListPlaceHolder . '' . $config->idListPlaceHolder . "';"}</script>
+  <script>{!echo "place" . md5(time()) . "='" . $config->idListPlaceHolder . '' . $config->idListPlaceHolder . "';"}</script>
 {/if}
 <div class='block-region region-top blocks' data-region='blog_index-top'>{$control->loadModel('block')->printRegion($layouts, 'blog_index', 'top')}</div>
 <hr class='space'>
 <div class='panel panel-section'>
   <div class='cards condensed cards-list bordered' id='blogList'>
     {foreach($sticks as $stick)}
-    {if(!isset($category)) $category = array_shift($stick->categories)}
-    {$url = inlink('view', "id=$stick->id", "category={$category->alias}&name=$stick->alias")}
-    <a class='card' href='{!echo $url?>' id="blog{!echo $stick->id?>" data-ve='blog'>
-      <div class='card-heading'>
-        <div class='pull-right'>
-          <small class='bg-danger-pale text-danger'>{!echo $lang->article->stick}</small>
-        </div>
-        <h5 style='color:{!echo $stick->titleColor}'>{!echo $stick->title?></h5>
-      </div>
-      <div class='table-layout'>
-        <div class='table-cell'>
-          <div class='card-content text-muted small'>
-            {!echo $stick->summary}
-            <div><span title="{!echo $lang->article->views}"><i class='icon-eye-open'></i> {!echo $config->viewsPlaceholder . $stick->id . $config->viewsPlaceholder}</span>
-                {if(commonModel::isAvailable('message') and isset($stick->comments) and $stick->comments)}&nbsp;&nbsp; <span title="{!echo $lang->article->comments}"><i class='icon-comments-alt'></i> {!echo $stick->comments}</span> &nbsp;{/if}
-                &nbsp;&nbsp; <span title="{!echo $lang->article->addedDate}"><i class='icon-time'></i> {!echo substr($stick->addedDate, 0, 10)}</span></div>
+      {if(!isset($category))} {$category = array_shift($stick->categories)} {/if}
+      {$url = inlink('view', "id=$stick->id", "category={{$category->alias}}&name=$stick->alias")}
+      <a class='card' href='{!echo $url?>' id="blog{!echo $stick->id?>" data-ve='blog'>
+        <div class='card-heading'>
+          <div class='pull-right'>
+            <small class='bg-danger-pale text-danger'>{$lang->article->stick}</small>
           </div>
+          <h5 style='color:{$stick->titleColor}'>{$stick->title?></h5>
         </div>
-        {if(!empty($stick->image))}
-        <div class='table-cell thumbnail-cell'>
-{*php*}
-          $title = $stick->image->primary->title ? $stick->image->primary->title : $stick->title;
-          echo html::image($control->loadModel('file')->printFileURL($stick->image->primary->pathname, $stick->image->primary->extension, 'article', 'smallURL'), "title='{$title}' class='thumbnail'");
-{*/php*}
+        <div class='table-layout'>
+          <div class='table-cell'>
+            <div class='card-content text-muted small'>
+              {$stick->summary}
+              <div><span title="{!echo $lang->article->views}"><i class='icon-eye-open'></i> {!$config->viewsPlaceholder . $stick->id . $config->viewsPlaceholder}</span>
+                {if(commonModel::isAvailable('message') and isset($stick->comments) and $stick->comments)}&nbsp;&nbsp; <span title="{$lang->article->comments}"><i class='icon-comments-alt'></i> {$stick->comments}</span> &nbsp;{/if}
+                &nbsp;&nbsp; <span title="{!echo $lang->article->addedDate}"><i class='icon-time'></i> {!substr($stick->addedDate, 0, 10)}</span></div>
+            </div>
+          </div>
+          {if(!empty($stick->image))}
+            <div class='table-cell thumbnail-cell'>
+              {$title = $stick->image->primary->title ? $stick->image->primary->title : $stick->title}
+              {!html::image($control->loadModel('file')->printFileURL($stick->image->primary->pathname, $stick->image->primary->extension, 'article', 'smallURL'), "title='{$title}' class='thumbnail'")}
+            </div>
+          {/if}
         </div>
-        {/if}
-      </div>
-    </a>
-    <?php unset($articles[$stick->id])}
+      </a>
+      <?php unset($articles[$stick->id])}
     {/foreach}
 
     {foreach($articles as $article)}
-    {if(!isset($category)) $category = array_shift($article->categories)}
-    {$url = inlink('view', "id=$article->id", "category={$category->alias}&name=$article->alias")}
-    <a class='card' href='{!echo $url?>' id="blog{!echo $article->id?>" data-ve='blog'>
-      <div class='card-heading'>
-        <h5 style='color:{!echo $article->titleColor}'>{!echo $article->title?></h5>
-      </div>
-      <div class='table-layout'>
-        <div class='table-cell'>
-          <div class='card-content text-muted small'>
-            {!echo $article->summary}
-            <div><span title="{!echo $lang->article->views}"><i class='icon-eye-open'></i> {!echo $config->viewsPlaceholder . $article->id . $config->viewsPlaceholder}</span>
-              {if(commonModel::isAvailable('message') and $article->comments)}&nbsp;&nbsp; <span title="{!echo $lang->article->comments}"><i class='icon-comments-alt'></i> {!echo $article->comments}</span> &nbsp;{/if}
-              &nbsp;&nbsp; <span title="{!echo $lang->article->addedDate}"><i class='icon-time'></i> {!echo substr($article->addedDate, 0, 10)}</span></div>
+      {if(!isset($category))} {$category = array_shift($article->categories)}{/if}
+      {$url = inlink('view', "id=$article->id", "category={{$category->alias}}&name=$article->alias")}
+      <a class='card' href='{!echo $url?>' id="blog{$article->id}" data-ve='blog'>
+        <div class='card-heading'>
+          <h5 style='color:{!echo $article->titleColor}'>{$article->title}</h5>
+        </div>
+        <div class='table-layout'>
+          <div class='table-cell'>
+            <div class='card-content text-muted small'>
+              {$article->summary}
+              <div><span title="{$lang->article->views}"><i class='icon-eye-open'></i> {!$config->viewsPlaceholder . $article->id . $config->viewsPlaceholder}</span>
+                {if(commonModel::isAvailable('message') and $article->comments)} &nbsp;&nbsp; <span title="{$lang->article->comments}"><i class='icon-comments-alt'></i> {$article->comments}</span> &nbsp;{/if}
+                &nbsp;&nbsp; <span title="{!$lang->article->addedDate}"><i class='icon-time'></i> {!substr($article->addedDate, 0, 10)}</span></div>
+            </div>
           </div>
+          {if(!empty($article->image))}
+            <div class='table-cell thumbnail-cell'>
+              {$title = $article->image->primary->title ? $article->image->primary->title : $article->title}
+              {!html::image($control->loadModel('file')->printFileURL($article->image->primary->pathname, $article->image->primary->extension, 'article', 'smallURL'), "title='{{$title}}' class='thumbnail'")}
+            </div>
+          {/if}
         </div>
-        {if(!empty($article->image))}
-        <div class='table-cell thumbnail-cell'>
-{*php*}
-          $title = $article->image->primary->title ? $article->image->primary->title : $article->title;
-          echo html::image($control->loadModel('file')->printFileURL($article->image->primary->pathname, $article->image->primary->extension, 'article', 'smallURL'), "title='{$title}' class='thumbnail'");
-{*/php*}
-        </div>
-        {/if}
-      </div>
-    </a>
+      </a>
     {/foreach}
   </div>
   <div class='panel-footer'>
