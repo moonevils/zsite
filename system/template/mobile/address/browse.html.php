5362991f8e5a7cb4cdfc9a8d3c5236e8060b8c15
wumo <wumo@cnezsoft.com>
2017-12-12 17:30:02 +0800

    * Change the file style to raintpl format.

--- system/template/mobile/address/browse.html.php
+++ system/template/mobile/address/browse.html.php
@@ -1,4 +1,4 @@
-{*
+{*php
 /**
  * The browse view file of address for mobile template of chanzhiEPS.
  *
@@ -9,7 +9,7 @@
  * @version     $Id$
  * @link        http://www.chanzhi.org
  */
-*}
+/php*}
 {include $control->loadModel('ui')->getEffectViewFile('mobile', 'common', 'header')}
 {include $control->loadModel('ui')->getEffectViewFile('mobile', 'user', 'side')}
 
@@ -24,27 +24,27 @@
     <div class='cards condensed cards-list' id='addressList'>
       {foreach($addresses as $address)}
         {$checked = isset($checked) ? '' : 'checked'}
-      <div class='card'>
-        <div class='card-heading'>
-          {if(helper::isAjaxRequest())} <input type='radio' {$checked} name='deliveryAddress' value='{$address->id}'/>{/if}
-          <strong class='lead'>{$address->contact}</strong>
-          &nbsp;&nbsp;<span class='text-special'><i class='icon icon-phone'></i>{str2Entity($address->phone)}</span>
+        <div class='card'>
+          <div class='card-heading'>
+            {if(helper::isAjaxRequest())} <input type='radio' {$checked} name='deliveryAddress' value='{$address->id}'/> {/if}
+            <strong class='lead'>{$address->contact}</strong>
+            &nbsp;&nbsp;<span class='text-special'><i class='icon icon-phone'></i>{echo str2Entity($address->phone)}</span>
+          </div>
+          <div class='card-content'>
+            {$address->address}<span class='text-muted'>({$lang->address->zipcode} : {$address->zipcode})</span>
+          </div>
+          <div class='card-footer'>
+            {!html::a(helper::createLink('address', 'edit', "id={{$address->id}}"), $lang->edit, "class='editor text-primary' data-toggle='modal'")}&nbsp;&nbsp}
+            {!html::a(helper::createLink('address', 'delete', "id={{$address->id}}"), $lang->delete, "class='deleter text-danger'")}
+          </div>
         </div>
-        <div class='card-content'>
-          {$address->address}<span class='text-muted'>({$lang->address->zipcode} : {$address->zipcode})</span>
-        </div>
-        <div class='card-footer'>
-          {!html::a(helper::createLink('address', 'edit', "id={$address->id}"), $lang->edit, "class='editor text-primary' data-toggle='modal'")}&nbsp;&nbsp;
-          {!html::a(helper::createLink('address', 'delete', "id={$address->id}"), $lang->delete, "class='deleter text-danger'")}
-        </div>
-      </div>
       {/foreach}
     </div>
   </div>
   {if(count($addresses) >= 5)}
-  <div class='panel-footer'>
-    <button type='button' class='btn primary block' data-toggle='modal' data-remote='{!inlink('create')}'><i class='icon icon-plus'></i> {$lang->address->create}</button>
-  </div>
+    <div class='panel-footer'>
+      <button type='button' class='btn primary block' data-toggle='modal' data-remote='{!inlink('create')}'><i class='icon icon-plus'></i> {$lang->address->create}</button>
+    </div>
   {/if}
 </div>
 {noparse}
