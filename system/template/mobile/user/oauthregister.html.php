53ef30a2008b797cb8ecce3a1dc37a401e0e1daf
wumo <wumo@cnezsoft.com>
2017-12-14 17:06:19 +0800

    * Change the file under the user module to raintpl format.

--- system/template/mobile/user/oauthregister.html.php
+++ system/template/mobile/user/oauthregister.html.php
@@ -1,30 +1,30 @@
 {include $control->loadModel('ui')->getEffectViewFile('mobile', 'common', 'header')}
 <div class='panel-section'>
-  <div class='panel-heading'><strong>{!echo $lang->user->oauth->lblProfile}</strong></div>
+  <div class='panel-heading'><strong>{$lang->user->oauth->lblProfile}</strong></div>
   <div class='panel-body'>
     <form method='post' class='ajaxform'>
       <div class='form-group'>
-        <label class='control-label' for='username'>{!echo $lang->user->account}</label>
-        {!echo html::input('account', '', "class='form-control' placeholder='{$lang->user->register->lblAccount}'")}
+        <label class='control-label' for='username'>{$lang->user->account}</label>
+        {!html::input('account', '', "class='form-control' placeholder='{{$lang->user->register->lblAccount}}'")}
       </div>
       <div class='form-group'>
-        <label class='control-label' for='realname'>{!echo $lang->user->realname}</label>
-        {!echo html::input('realname', '', "class='form-control'")}
+        <label class='control-label' for='realname'>{$lang->user->realname}</label>
+        {!html::input('realname', '', "class='form-control'")}
       </div>
       <div class='form-group'>
-        <label class='control-label' for='email'>{!echo $lang->user->email}</label>
-        {!echo html::input('email', '', "class='form-control'")}
+        <label class='control-label' for='email'>{$lang->user->email}</label>
+        {!html::input('email', '', "class='form-control'")}
       </div>
       <div class='form-group'>
-        <label class='control-label' for='password'>{!echo $lang->user->password}</label>
-        {!echo html::password('password1', '', "class='form-control'")}
+        <label class='control-label' for='password'>{$lang->user->password}</label>
+        {!html::password('password1', '', "class='form-control'")}
       </div>
       <div class='form-group'>
-        <label class='control-label' for='password'>{!echo $lang->user->password2}</label>
-        {!echo html::password('password2', '', "class='form-control'")}
+        <label class='control-label' for='password'>{$lang->user->password2}</label>
+        {!html::password('password2', '', "class='form-control'")}
       </div>
       <div class='form-group'>
-        {!echo html::submitButton('', 'btn block success') . html::hidden('referer', $referer)}
+        {!html::submitButton('', 'btn block success') . html::hidden('referer', $referer)}
       </div>
     </form>
   </div>
