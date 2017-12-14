5bf90a401d27f18b59c10551fca5ca007cae6177
Xiying Guan <guanxiying@cnezsoft.com>
2017-12-14 13:39:53 +0800

    * Commit temp.

--- system/template/mobile/user/profile.html.php
+++ system/template/mobile/user/profile.html.php
@@ -28,50 +28,52 @@
       </td>
     </tr>
     <tr>
-      <th>{!echo $lang->user->email}</th>
-      <td>{if(!empty($user->email)) echo str2Entity($user->email)}</td>
+      <th>{$lang->user->email}</th>
+      <td>{if(!empty($user->email))} {!str2Entity($user->email)} {/if}</td>
     </tr>
     <tr>
-      <th>{!echo $lang->user->company}</th>
-      <td>{!echo $user->company}</td>
+      <th>{$lang->user->company}</th>
+      <td>{$user->company}</td>
     </tr>
     <tr>
-      <th>{!echo $lang->user->address}</th>
-      <td>{!echo $user->address}</td>
+      <th>{$lang->user->address}</th>
+      <td>{$user->address}</td>
     </tr>
     <tr>
-      <th>{!echo $lang->user->zipcode}</th>
-      <td>{!echo $user->zipcode}</td>
+      <th>{$lang->user->zipcode}</th>
+      <td>{$user->zipcode}</td>
     </tr>
     <tr>
-      <th>{!echo $lang->user->mobile}</th>
-      <td>{!echo str2Entity($user->mobile)}</td>
+      <th>{$lang->user->mobile}</th>
+      <td>{!str2Entity($user->mobile)}</td>
     </tr>
     <tr>
-      <th>{!echo $lang->user->phone}</th>
-      <td>{!echo str2Entity($user->phone)}</td>
+      <th>{$lang->user->phone}</th>
+      <td>{!str2Entity($user->phone)}</td>
     </tr>
     <tr>
-      <th>{!echo $lang->user->qq}</th>
-      <td>{!echo str2Entity($user->qq)}</td>
+      <th>{$lang->user->qq}</th>
+      <td>{!str2Entity($user->qq)}</td>
     </tr>
     <tr>
-      <th>{!echo $lang->user->gtalk}</th>
-      <td>{!echo $user->gtalk}</td>
+      <th>{$lang->user->gtalk}</th>
+      <td>{$user->gtalk}</td>
     </tr>
     <tr>
       <td colspan='2'>
         <div class='row'>
-          <div class='col-6'>{!echo html::a(inlink('edit'), "<i class='icon-pencil'></i> " . $lang->user->editProfile, "class='btn block primary' data-toggle='modal'")}</div>
-          <div class='col-6'>{!echo html::a(inlink('setemail'), "<i class='icon-envelope'></i> " . $lang->user->setEmail, "class='btn block primary' data-toggle='modal'")}</div>
+          <div class='col-6'>{!html::a(inlink('edit'), "<i class='icon-pencil'></i> " . $lang->user->editProfile, "class='btn block primary' data-toggle='modal'")}</div>
+          <div class='col-6'>{!html::a(inlink('setemail'), "<i class='icon-envelope'></i> " . $lang->user->setEmail, "class='btn block primary' data-toggle='modal'")}</div>
           {if(isset($user->provider) and isset($user->openID))}
-          {if(strpos($user->account, "{$user->provider}_") === false)}
-          <div class='col-6'>{!echo html::a(inlink('oauthUnbind', "account=$user->account&provider=$user->provider&openID=$user->openID"), "<i class='icon-unlink'></i> " . $lang->user->oauth->lblUnbind, "class='btn block primary ajaxaction jsoner'")}</div>
-          {else}
-          <br>
-          <div class='col-6'>{!echo html::a(inlink('oauthRegister'), "<i class='icon-link'></i> " . $lang->user->oauth->lblProfile, "class='btn block primary'")}</div>
-          <div class='col-6'>{!echo html::a(inlink('oauthBind'), "<i class='icon-link'></i> " . $lang->user->oauth->lblBind, "class='btn block primary'")}</div>
-          {/if}
+            {if(strpos($user->account, "{{$user->provider}}_") === false)}
+              <div class='col-6'>
+                {!html::a(inlink('oauthUnbind', "account=$user->account&provider=$user->provider&openID=$user->openID"), "<i class='icon-unlink'></i> " . $lang->user->oauth->lblUnbind, "class='btn block primary ajaxaction jsoner'")}
+              </div>
+            {else}
+              <br>
+              <div class='col-6'>{!html::a(inlink('oauthRegister'), "<i class='icon-link'></i> " . $lang->user->oauth->lblProfile, "class='btn block primary'")}</div>
+              <div class='col-6'>{!html::a(inlink('oauthBind'), "<i class='icon-link'></i> " . $lang->user->oauth->lblBind, "class='btn block primary'")}</div>
+            {/if}
           {/if}
         </div>
       </td>
