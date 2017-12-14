ff1417778299420bc2417f8989bf72fc291f7858
wumo <wumo@cnezsoft.com>
2017-12-14 13:33:21 +0800

    * Change the file under rrs, search and errors module.

--- system/template/mobile/rss/index.xml.php
+++ system/template/mobile/rss/index.xml.php
@@ -1,33 +1,30 @@
-{*php*}
+{*php
 $webRoot            = $config->webRoot;
 $templateName       = $control->config->template->{$control->app->clientDevice}->name;
 $templateRoot       = $webRoot . "template/{$templateName}/";
 $templateThemeRoot  = "{$templateRoot}theme/";
 $templateCommonRoot = "{$templateThemeRoot}common/";
-{*/php*}
+/php*}
 {!echo '<?xml version="1.0" encoding="utf-8"?>'}
-{!echo '<?xml-stylesheet type="text/css" href="' . $templateCommonRoot . 'css/rss.css" ?>' ?>
+{!echo '<?xml-stylesheet type="text/css" href="' . $templateCommonRoot . 'css/rss.css" ?>' }
 <rss version="2.0">
 <channel>
-  <title>{!echo $title}</title>
-  <link>{!echo $siteLink}</link>
-  <description>{!echo $desc}</description>
+  <title>{$title}</title>
+  <link>{$siteLink}</link>
+  <description>{$desc}</description>
   <copyright>{!echo $config->company->name . $config->site->copyright . '-' . date('Y')}</copyright>
-  <lastBuildDate>{!echo $lastDate}</lastBuildDate>
-  
-{*php*}
-  foreach($articles as $article):
-    $category = current($article->categories);
-    $article->content = str_replace('src="/data/upload/', 'src="' . getWebRoot(true) . 'data/upload/', $article->content);
-    $article->content = str_replace("src='/data/upload/", "src='" . getWebRoot(true) . 'data/upload/', $article->content);
-{*/php*}
-  <item>
-    <title>{!echo $article->title?></title>
-    <description><![CDATA[  {!echo $article->content}]]></description>
-    <link>{!echo str_replace('&', '&amp;', $siteLink . $control->createLink('blog', 'view', "id=$article->id", "category={$category->alias}&name=$article->alias", 'html'))}</link>
-    <category>{!echo $category->name}</category>
-    <pubDate>{!echo $article->addedDate . ' +0800'}</pubDate>
-  </item>
+  <lastBuildDate>{$lastDate}</lastBuildDate>
+  {foreach($articles as $article)}
+    {$category = current($article->categories)}
+    {$article->content = str_replace('src="/data/upload/', 'src="' . getWebRoot(true) . 'data/upload/', $article->content)}
+    {$article->content = str_replace("src='/data/upload/", "src='" . getWebRoot(true) . 'data/upload/', $article->content)}
+    <item>
+      <title>{$article->title}</title>
+      <description><![CDATA[{$article->content}]]></description>
+      <link>{!str_replace('&', '&amp;', $siteLink . $control->createLink('blog', 'view', "id=$article->id", "category={{$category->alias}}&name=$article->alias", 'html'))}</link>
+      <category>{$category->name}</category>
+      <pubDate>{!echo $article->addedDate . ' +0800'}</pubDate>
+    </item>
   {/foreach}
 </channel>
 </rss>
