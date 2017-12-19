9347a3f80569132f8df925cf490d27cfc84dabc5
wumo <wumo@cnezsoft.com>
2017-12-19 14:57:51 +0800

    * Ajust the style of checkemail page.

--- system/template/default/user/checkemail.html.php
+++ system/template/default/user/checkemail.html.php
@@ -1,26 +1,24 @@
 {include $control->loadModel('ui')->getEffectViewFile('default', 'common', 'header')}
-<div class='panel panel-body'>
-  <div class='panel panel-pure' id='checkEmail'>
-    <div class='panel-heading'><strong>{!echo $lang->user->checkEmail}</strong></div>
-    <div class='panel-body'>
-      <form method='post' action='{!echo inlink('checkEmail')}' id='ajaxForm'>
-        <table class='table table-form'>
-          <tr>
-            <th>{!echo $lang->user->email}</th>
-            <td>{!echo html::input('email', $user->email, "class='form-control'")}</td>
-            <td>{!echo html::a($control->createLink('mail', 'sendmailcode'), $lang->user->getEmailCode, "id='mailSender' class='btn btn-xs'")}</td>
-          </tr>
-          <tr>
-            <th>{!echo $lang->user->captcha}</th>
-            <td>{!echo html::input('captcha', '', "class='form-control'")}</td>
-          </tr>
-          <tr>
-            <th></th>
-            <td>{!echo html::submitButton() . html::hidden('referer', $referer)}</td>
-          </tr>
-        </table>
-      </form>
-    </div>
+<div class='panel' id='checkEmail'>
+  <div class='panel-heading'><strong>{$lang->user->checkEmail}</strong></div>
+  <div class='panel-body'>
+    <form method='post' action='{!inlink('checkEmail')}' id='ajaxForm'>
+      <table class='table table-form'>
+        <tr>
+          <th>{!$lang->user->email}</th>
+          <td>{!html::input('email', $user->email, "class='form-control'")}</td>
+          <td>{!html::a($control->createLink('mail', 'sendmailcode'), $lang->user->getEmailCode, "id='mailSender' class='btn btn-xs'")}</td>
+        </tr>
+        <tr>
+          <th>{$lang->user->captcha}</th>
+          <td>{!html::input('captcha', '', "class='form-control'")}</td>
+        </tr>
+        <tr>
+          <th></th>
+          <td>{!html::submitButton() . html::hidden('referer', $referer)}</td>
+        </tr>
+      </table>
+    </form>
   </div>
 </div>
 {include $control->loadModel('ui')->getEffectViewFile('default', 'common', 'footer')}
