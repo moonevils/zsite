d7579249224d077e4317a1b950fa6360d24e5fd7
Xiying Guan <guanxiying@cnezsoft.com>
2017-12-19 13:15:09 +0800

    * Fix bugs.

--- system/template/default/block/latestbook.block.html.php
+++ system/template/default/block/latestbook.block.html.php
@@ -1,19 +1,19 @@
 <div class='panel-body'>
   <div class='cards cards-custom'>
     {foreach($books as $book)}
-    {$recPerRow = $content->recPerRow}
-    <div class='pull-left with-padding' style="width:{!echo 100 / $recPerRow}%" data-recperrow="{!echo $recPerRow}">
-      <div class='card with-margin'>
-        <div class='card-heading text-center'>
-          {!echo html::a(helper::createLink('book', 'browse', "nodeID=$book->id", "book=$book->alias") . ($control->get->fullScreen ? "?fullScreen={$control->get->fullScreen}" : ''), $book->title)}
-        </div>
-        <div class='card-content text-muted text-center'>{!echo $book->content}</div>
-        <div class='card-actions'>
-          <span class='text-muted'><i class='icon-user'></i> {!echo $book->author}</span>
-          <span class='text-muted'><i class='icon-time'></i> {!echo formatTime($book->addedDate, 'Y-m-d')}</span>
+      {$recPerRow = $content->recPerRow}
+      <div class='pull-left with-padding' style="width:{!echo 100 / $recPerRow}%" data-recperrow="{$recPerRow}">
+        <div class='card with-margin'>
+          <div class='card-heading text-center'>
+            {!html::a(helper::createLink('book', 'browse', "nodeID=$book->id", "book=$book->alias") . ($model->get->fullScreen ? "?fullScreen={{$model->get->fullScreen}}" : ''), $book->title)}
+          </div>
+          <div class='card-content text-muted text-center'>{!strip_tags(htmlspecialchars_decode($book->content))}</div>
+          <div class='card-actions'>
+            <span class='text-muted'><i class='icon-user'></i> {$book->author}</span>
+            <span class='text-muted'><i class='icon-time'></i> {!formatTime($book->addedDate, 'Y-m-d')}</span>
+          </div>
         </div>
       </div>
-    </div>
     {/foreach}
   </div>
 </div>
