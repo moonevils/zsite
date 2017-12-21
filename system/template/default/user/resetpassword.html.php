4a4070703c00037eaed1c01696ad1fdf28fa855a
wumo <wumo@cnezsoft.com>
2017-12-21 09:52:20 +0800

    * Fix the style of reset password page.

--- system/template/default/user/resetpassword.html.php
+++ system/template/default/user/resetpassword.html.php
@@ -1,6 +1,6 @@
 {include $control->loadModel('ui')->getEffectViewFile('default', 'common', 'header')}
 <div class='panel panel-body'>
-  <div class='panel panel-pure' id='reset'>
+  <div class='panel' id='reset'>
     <div class='panel-heading'><strong>{$lang->user->sendRecoverEmail}</strong></div>
     <div class='panel-body'>
       <form method='post' id='ajaxForm'>
