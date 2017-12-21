8e8c1c25519469eaf215419083b9651ba430cdcd
wumo <wumo@cnezsoft.com>
2017-12-21 09:48:47 +0800

    * Fix the bug of login.

--- system/template/default/user/login.html.php
+++ system/template/default/user/login.html.php
@@ -9,7 +9,7 @@
 {!js::set('random', $control->session->random)}
 <div class='panel panel-body' id='login'>
   <div class='row'>
-    <div class='panel panel-pure' id='login-pure'>
+    <div class='panel' id='login-pure'>
       <div id='login-region'>
         <div class='panel-heading'><span>{$lang->user->login->welcome}</span></div>
         <div class='panel-body'>
@@ -19,16 +19,16 @@
             <div class='form-group'>{!html::password('password','',"placeholder='{{$lang->user->inputPassword}}' class='form-control input-lg'")}</div>
             {!html::submitButton($lang->user->login->common, 'btn btn-primary btn-wider btn-lg btn-block')} &nbsp; &nbsp; 
             {!html::hidden('referer', $referer)}
+            {include TPL_ROOT . 'user/oauthlogin.html.php'}
+            <span class='regAndReset pull-right'>
+            {if($config->mail->turnon and $control->config->site->resetPassword == 'open')}
+              {!html::a(inlink('resetpassword'), $lang->user->recoverPassword, "id='reset-pass'")}
+            {/if}
+            {if(!$formOnly)}
+              {!html::a(inlink('register'), $lang->user->register->instant, "id='register'")}
+            {/if}
+            </span>
           </form>
-          {include TPL_ROOT . 'user/oauthlogin.html.php'}
-          <span class='regAndReset pull-right'>
-          {if($config->mail->turnon and $control->config->site->resetPassword == 'open')}
-          {!html::a(inlink('resetpassword'), $lang->user->recoverPassword, "id='reset-pass'")}
-          {/if}
-          {if(!$formOnly)}
-          {!html::a(inlink('register'), $lang->user->register->instant, "id='register'")}
-          {/if}
-          </span>
         </div>
       </div>
     </div>
