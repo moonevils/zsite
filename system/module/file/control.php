b1399139501f550b70c9d45e1b67bc7da5ed9836
wumo <wumo@cnezsoft.com>
2018-01-29 11:38:08 +0800

    * Fix the bug of image compress.

--- system/module/file/control.php
+++ system/module/file/control.php
@@ -731,9 +731,10 @@ class file extends control
         $rawImages  = array_slice($images, $lastImage, $limit);
         $completed += $limit;
 
+        $allFiles = $this->dao->select('pathname')->from(TABLE_FILE)->fetchAll();
         foreach($rawImages as $image)
         {
-            $extension = $this->file->getExtension($image);
+            $extension = $this->file->getThumbsExtension($image,$allFiles);
             if(in_array(strtolower($extension), $this->config->file->imageExtensions, true) === false) continue;
             $this->file->compressImage($image);
         }
