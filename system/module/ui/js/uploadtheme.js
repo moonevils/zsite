1c73ceeabb5ff342f5000ce7f968e63031ca5285
wumo <wumo@cnezsoft.com>
2018-03-02 14:36:04 +0800

    * Modify popover prompt time.

--- system/module/ui/js/uploadtheme.js
+++ system/module/ui/js/uploadtheme.js
@@ -10,7 +10,7 @@ $(document).ready(function()
                 {
                     $.ajustModalPosition();
                 });
-            }, 2000);
+            }, 3000);
         }
     });
 
