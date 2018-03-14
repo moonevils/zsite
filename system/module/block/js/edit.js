7d5b68766608521e0fe21e12819f3f0563b386df
guanxiying <guanxiying@xirangit.com>
2018-03-14 08:57:00 +0800

    * Remove unused codes.

--- system/module/block/js/edit.js
+++ system/module/block/js/edit.js
@@ -13,10 +13,6 @@ $(document).ready(function()
         }   
     });
 
-    $('#type').change(function()
-    {
-        location.href = createLink('block', 'edit', 'id=' + v.id + '&type=' + $(this).val() );
-    });
     $.setAjaxForm('#editForm', function(response)
     {   
         if(response.result == 'fail' && response.reason == 'captcha')
