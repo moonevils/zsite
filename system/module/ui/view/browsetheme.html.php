a8203edefcbba59b12115414a823f387642c6849
estellbright <1457749147@qq.com>
2018-08-10 11:25:09 +0800

    Fix bugs.

--- system/module/ui/view/browsetheme.html.php
+++ system/module/ui/view/browsetheme.html.php
@@ -28,7 +28,7 @@
 <div class='div-tip text-danger'><?php printf($lang->ui->packagePathTip, $packagePath);?></div>
 <style>
 .theme-panel > .panel-body{padding-top:4px !important; cursor:pointer;}
-.theme-panel > .panel-body > .theme-title{font-size:16px; padding: 10px 0; color:#555; font-weight:bold;}
+.theme-panel > .panel-body > .theme-title{font-size:16px; padding: 10px 0; color:#555; font-weight:bold;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;}
 .theme-panel .span-size{margin-left:20px;}
 .p-actions{margin-right:10px; padding-left:12px;}
 .p-actions > i {font-size:13px; padding:4px; font-weight:bold;}
