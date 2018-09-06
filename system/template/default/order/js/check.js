6d55cf234ee75c616333345e03805e2ceaac65a0
daitingting <daitingting@xirangit.com>
2018-09-06 11:29:59 +0800

    * Locate directly if device is desktop.

--- system/template/default/order/js/check.js
+++ system/template/default/order/js/check.js
@@ -2,33 +2,30 @@ $(document).ready(function()
 {
     $('[name=payment]').eq(0).prop('checked', true);
 
-    $.setAjaxForm('#checkForm', function(response)
+    $('#submit').click(function()
     {
-        if(response.result == 'success')
+        var payment = $('input:radio[name=payment]:checked').val();
+        if(payment == 'COD')
         {
-            if(response.payment == 'COD')
-            {
-                location.href = response.locate;
-            }
-            else
-            {
-                $('.locate2pay').attr('href', response.locate);;
-                $('.locate2pay')[0].click();
+            $('#checkForm').attr('target', '');
+        }
+        else
+        {
+            $('#checkForm').attr('target', '_blank');
 
-                bootbox.dialog(
+            bootbox.dialog(
+            {  
+                message: v.goToPay,  
+                buttons:
                 {  
-                    message: v.goToPay,  
-                    buttons:
-                    {  
-                        paySuccess:
-                        {
-                            label:     v.paid,  
-                            className: 'btn-primary',  
-                            callback:  function() { setTimeout(function(){location.href = createLink('order', 'browse');}, 600); }  
-                        }
+                    paySuccess:
+                    {
+                        label:     v.paid,  
+                        className: 'btn-primary',  
+                        callback:  function() { setTimeout(function(){location.href = createLink('order', 'browse');}, 600); }  
                     }
-                });
-            }
+                }
+            });
         }
     });
 });
