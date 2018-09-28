f1b4235d3fe0abf7fa0d7e447db0ae98973b894e
guanxiying <guanxiying@xirangit.com>
2018-09-28 09:58:07 +0800

    * Add bear path.

---
+++ system/module/bear/lang/zh-cn.php
@@ -0,0 +1,38 @@
+<?php
+$lang->bear->common     = '熊掌号';
+$lang->bear->setting    = '熊掌号设置';
+$lang->bear->name       = '熊掌号名称';
+$lang->bear->type       = '类型';
+$lang->bear->appID      = 'appID';
+$lang->bear->token      = 'Token';
+$lang->bear->autoSync   = '自动同步';
+$lang->bear->submitType = '推送类型';
+
+$lang->bear->submit = '提交资源到百度';
+$lang->bear->submitSuccess = '资源提交成功';
+$lang->bear->submitFail    = '资源提交失败';
+
+$lang->bear->notices = array();
+$lang->bear->notices['not_same_site'] = "%s不是本站的url。";
+$lang->bear->notices['not_valid']     = "不合法的url列表。";
+
+$lang->bear->submitTypes = array();
+$lang->bear->submitTypes['realtime'] = '新增';
+$lang->bear->submitTypes['batch']    = '历史';
+
+$lang->bear->syncObjects = new stdclass;
+$lang->bear->syncObjects->article = '文章';
+$lang->bear->syncObjects->product = '产品';
+$lang->bear->syncObjects->blog    = '博客';
+$lang->bear->syncObjects->page    = '单页';
+
+$lang->bear->placeholder = new stdclass;
+$lang->bear->placeholder->appID = '您的熊掌号唯一识别ID';
+$lang->bear->placeholder->token = '在搜索资源平台申请的推送用的准入密钥';
+
+$lang->bear->typeList = array();
+$lang->bear->typeList[1] = '个人';
+$lang->bear->typeList[2] = '媒体';
+$lang->bear->typeList[3] = '企业';
+$lang->bear->typeList[4] = '政府';
+$lang->bear->typeList[5] = '其他组织';
