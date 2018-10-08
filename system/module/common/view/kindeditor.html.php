2db89065d3d7fd5f5591c74bf534b398f96bfedf
Catouse <catouse@gmail.com>
2018-10-08 17:00:32 +0800

    * code for task #2875, improve kindeditor UI.

--- system/module/common/view/kindeditor.html.php
+++ system/module/common/view/kindeditor.html.php
@@ -25,9 +25,8 @@ $editorLang  = isset($editorLangs[$app->getClientLang()]) ? $editorLangs[$app->g
 js::set('editorLang', $editorLang);
 
 /* Import css and js for kindeditor. */
-js::import($jsRoot  . 'kindeditor/kindeditor-min.js');
+js::import($jsRoot  . 'kindeditor/kindeditor.min.js');
 js::import($jsRoot  . 'kindeditor/lang/' . $editorLang . '.js');
-js::import($jsRoot  . 'kindeditor/plugins/table/tablex.js');
 
 /* set uid for upload. */
 $uid = uniqid('');
@@ -55,7 +54,7 @@ $.extend(KindEditor, {getOptions: function(K, editorID)
         width:'100%',
         items: editorTool,
         cssPath:[v.webRoot + 'zui/css/min.css'],
-        cssData: '.article-content, .article-content table td, .article-content table th {line-height: 1.4285714286} .article-content table.table-kindeditor th, .article-content table.table-kindeditor td {padding: 5px 8px;} .article-content table.ke-zeroborder.table-kindeditor td {border: 1px dotted #AAA;} .article-content .table-auto {width: auto; max-width: 100%;}',
+        cssData: '.article-content, .article-content table td, .article-content table th {line-height: 1.3846153846; font-size: 13px;} .article-content table.table-kindeditor th, .article-content table.table-kindeditor td {padding: 5px 8px;} .article-content table.ke-zeroborder.table-kindeditor td {border: 1px dotted #AAA;} .article-content .table-auto {width: auto; max-width: 100%;}',
         bodyClass:'article-content',
         urlType:'absolute',
         uploadJson: createLink('file', 'ajaxUpload', 'uid=' + v.uid),
@@ -74,7 +73,7 @@ $.extend(KindEditor, {getOptions: function(K, editorID)
         font:["id","class","color","size","face",".background-color"],span:["id","class",".color",".background-color",".font-size",".font-family",".background",".font-weight",".font-style",".text-decoration",".vertical-align",".line-height"],div:["id","class","align",".border",".margin",".padding",".text-align",".color",".background-color",".font-size",".font-family",
         ".font-weight",".background",".font-style",".text-decoration",".vertical-align",".margin-left"],table:["id","class","border","cellspacing","cellpadding","width","height","align","bordercolor",".padding",".margin",".border","bgcolor",".text-align",".color",".background-color",".font-size",".font-family",".font-weight",".font-style",".text-decoration",".background",".width",".height",".border-collapse"],"td,th":["id","class","align","valign","width","height","colspan","rowspan","bgcolor",".text-align",
         ".color",".background-color",".font-size",".font-family",".font-weight",".font-style",".text-decoration",".vertical-align",".background",".border"],a:["id","class","href","target","name"],embed:["id","class","src","width","height","type","loop","autostart","quality",".width",".height","align","allowscriptaccess","allowfullscreen"],img:["id","class","src","width","height","border","alt","title","align",".width",".height",".border"],"p,ol,ul,li,blockquote,h1,h2,h3,h4,h5,h6":["id","class","align",".text-align",".color",
-        ".background-color",".font-size",".font-family",".background",".font-weight",".font-style",".text-decoration",".vertical-align",".text-indent",".margin-left"],pre:["id","class"],hr:["id","class",".page-break-after"],"br,tbody,tr,strong,b,sub,sup,em,i,u,strike,s,del":["id","class"],iframe:["id","class","src","frameborder","width","height",".width",".height"],style:[]},
+        ".background-color",".font-size",".font-family",".background",".font-weight",".font-style",".text-decoration",".vertical-align",".text-indent",".margin-left"],pre:["id","class"],hr:["id","class",".page-break-after"],"br,tbody,thead,tr,strong,b,sub,sup,em,i,u,strike,s,del":["id","class"],iframe:["id","class","src","frameborder","width","height",".width",".height"],style:[]},
         afterBlur: function(){this.sync();$('#' + editorID).prev('.ke-container').removeClass('focus');},
         afterFocus: function(){$('#' + editorID).prev('.ke-container').addClass('focus');},
         afterChange: function(){$('#' + editorID ).change().hide();},
