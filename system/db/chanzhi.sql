dadeaa12093bf8b1f68da22efa20574a032fc549
guanxiying <guanxiying@xirangit.com>
2018-10-08 15:10:16 +0800

    * Fix sql of bearlog.

--- system/db/chanzhi.sql
+++ system/db/chanzhi.sql
@@ -845,6 +845,28 @@ CREATE TABLE IF NOT EXISTS `eps_address` (
   KEY `lang` (`lang`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- DROP TABLE IF EXISTS `eps_bearlog`;
+CREATE TABLE `eps_bearlog` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(10) NOT NULL,
+  `objectType` varchar(30) NOT NULL,
+  `objectID` mediumint(9) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `account` varchar(30) NOT NULL,
+  `status` char(30) NOT NULL,
+  `response` text NOT NULL,
+  `time` datetime NOT NULL,
+  `auto` enum('yes','no') NOT NULL,
+  `lang` char(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account` (`account`),
+  KEY `lang` (`lang`),
+  KEY `objectType` (`objectType`),
+  KEY `objectID` (`objectID`),
+  KEY `time` (`time`),
+  KEY `type` (`type`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `eps_blacklist`;
 CREATE TABLE IF NOT EXISTS  `eps_blacklist` (
   `type` varchar(30) NOT NULL,
