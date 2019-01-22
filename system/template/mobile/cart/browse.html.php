6b0d60dca76f5b68746482d9340eb7fe9164b36a
wangyuting2 <851424971@qq.com>
2019-01-22 15:26:19 +0800

    * Adjust cart and product css.

--- system/template/mobile/cart/browse.html.php
+++ system/template/mobile/cart/browse.html.php
@@ -14,10 +14,11 @@
 {!js::set('currencySymbol', $currencySymbol)}
 <style>
   body {background:#f6f6f6}
-  .panel.panel-section {border:0px}
-  .panel-section .panel-heading.page-header {margin-top:0px} 
-  .cards.condensed {background:#f6f6f6;padding-bottom:1px} 
-  .cards.condensed .card {margin:10px}
+  .panel.panel-section {border:0px;margin:0px}
+  .panel-section .panel-heading.page-header {margin-top:0px;height:44px;line-height:44px;padding:0px 10px} 
+  .panel-section .panel-heading.page-header .title {color:#999} 
+  .cards.condensed.cards-list {background:#f6f6f6;padding-bottom:1px;overflow:hidden;padding:0px} 
+  .cards.condensed .card {margin:10px;padding:0px}
   .input-group.input-group-sm.input-number {width:100px;float:right}
   .input-group input {text-align:center;height:32px}
   .media-placeholder {line-height:120px;padding:0px}
@@ -28,14 +29,25 @@
   .table-layout > tbody > tr > td, .table-layout > tbody > tr > th, .table-layout > tfoot > tr > td, .table-layout > tfoot > tr > th, .table-layout > thead > tr >t d, .table-layout > thead > tr > th {padding-right:0px;padding-left:0px}
   .text-danger {color:#D0021B}
   .input-group-btn .btn.default {padding:5px 10px}
-  .checkarea {float:left;width:8%;text-align:center;margin-top:50px;position: relative}
+  .checkarea {float:left;width:8%;text-align:center;margin-top:45px;position: relative}
   .opt {float:right;font-size:1.5rem}
   .total {display: table-cell;vertical-align: middle;height:40px}
   .checkarea > label {position: relative;z-index: 0;padding: .25rem 0 .25rem 1.8rem}
   .checkarea > label:after, .checkarea > label:before {position: absolute;top: .3rem;left: 0;display: -ms-flexbox;display: flex;width: 1.5rem;height: 1.5rem;content: ' ';transition: .2s cubic-bezier(.175,.885,.32,1);color: transparent;border: .1rem solid rgba(0,0,0,.5);-ms-flex-align: center;align-items: center;-ms-flex-pack: center;justify-content: center;border-radius: 1.5rem}
+  .footer-left > .checkarea > label:after, .footer-left > .checkarea > label:before {top:16px}
   .checkarea > input:checked + label:after, .checkarea > input:checked + label:before {color: #fff;border-radius: .1rem;background-color: #3280fc;border-radius: 1.5rem}
   .checkarea > label:after {font-family: ZenIcon;font-size: 1.5rem;font-weight: 400;font-style: normal;font-variant: normal;content: '\e60d';text-transform: none;border: none;speak: none;-webkit-font-smoothing: antialiased}
   .checkarea > input {position: absolute;z-index: 1;top: 0;left: 0;display: block;width: 100%;height: 100%;opacity: 0;}
+  #footerNav {line-height:49px;height:49px}
+  .footer-left {width:20%;float:left;padding-left:12px}
+  .footer-left > a > img {width:20px;height:20px}
+  .footer-left > a > span {position:absolute;z-index:1;top:-.8rem;right:-.8rem;width:18px;height:18px;text-align:center;line-height:18px}
+  .footer-right {width:80%;float:right;}
+  .footer-right > .right-btn {width:41.5%;float:right;margin-right:12px}
+  .footer-right > .right-btn > .total > span {font-size:1rem}
+  .footer-right > .right-btn > button,input {width:100%;line-height:28px;height:30px;border-radius:4px;padding:0px;outline:none}
+  .footer-right > .right-btn > .btn-order-delete {border:1px solid #6F9AFE;color:#6F9AFE;background-color:#fff}
+  .footer-right > .right-btn > .btn-order-submit {border:1px solid #6F9AFE;color:#fff;background:linear-gradient(to right,#709BFE,#1B5AFF)}
 </style>
 <div class='panel panel-section'>
   <div class='panel-heading page-header'>
@@ -124,29 +136,23 @@
       {/foreach}
       </div>
       <footer class="appbar fix-bottom" id='footerNav' data-ve='navbar' data-type='mobile_bottom'>
-      <ul class="nav">
-          <li style='top:-6px'>
-            <div class='col-12'>
-              <div class='checkarea' style='margin-top:0px;padding-left:5%;width:100%;text-align:left'>
-                <input type='checkbox' id='checkAll'>
-                <label style='width:50px'>{$lang->selectAll}</label>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class='col-8'>
-              <div style='float:right'>
-                <div class='total'>
-                  <span>{!printf($lang->order->statistics, 0, $currencySymbol . '0')}</span>
-                </div>
-              </div>
-            </div>
-            <div class='col-4'>
-              {!html::submitButton($lang->cart->goAccount, 'btn-order-submit btn block primary')}
-              <input type='button' class='btn-order-delete btn block light' style='display:none;margin-top:0px' value='{$lang->delete}'></input>
-            </div>
-          </li>
-      </ul>
+      <div class='footer-left'>
+        <div class='checkarea' style='margin-top:0px;padding-left:0px;width:100%;text-align:left'>
+            <input type='checkbox' id='checkAll'>
+            <label style='width:50px'>{$lang->selectAll}</label>
+        </div>
+      </div>          
+      <div class='footer-right'>
+        <div class='right-btn'>
+          {!html::submitButton($lang->cart->goAccount, 'btn-order-submit')}
+          <button type='button' class='btn-order-delete hide'>{$lang->delete}</button>
+        </div>
+        <div class='right-btn' style='margin-right:5px;width:auto'>
+          <div class='total'>
+            <span>{!printf($lang->order->statistics, 0, $currencySymbol . '0')}</span>
+          </div>
+        </div>
+      </div>
       </footer>
     </form>
   {else}
