d6d6fc5507e61ae6de317cc7300a91bb6289b58b
wangyuting2 <851424971@qq.com>
2019-01-28 08:44:06 +0800

    * Adjust address css.

--- system/template/mobile/address/edit.html.php
+++ system/template/mobile/address/edit.html.php
@@ -37,3 +37,21 @@
     </div>
   </div>
 </div>
+{noparse}
+<script>
+$(function()
+{
+    var $editForm = $('#editForm');
+    $editForm.ajaxform({onResultSuccess: 
+    function(response)
+    {
+        $.closeModal();
+        if($.isFunction($.refreshAddressList))
+        {
+            setTimeout($.refreshAddressList, 200);
+            response.locate = false;
+        }
+    } });
+});
+</script>
+{/noparse}
