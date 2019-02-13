157bde00986ae7064174bf87abf3804d868d89e8
wangyuting2 <851424971@qq.com>
2019-02-13 14:09:11 +0800

    * Adjust my reply page css.

--- system/template/mobile/user/reply.html.php
+++ system/template/mobile/user/reply.html.php
@@ -11,19 +11,32 @@
  */
 /php*}
 {include $control->loadModel('ui')->getEffectViewFile('mobile', 'common', 'header.simple')}
-
+<style>
+.panel > .panel-heading, .panel-section > .panel-heading {padding:12px 10px 0px 10px}
+.cards.condensed .card-fix {width:100%}
+.card .card-top {width:100%;overflow:hidden;margin-bottom:8px}
+.card .card-body {width:100%;overflow:hidden;margin-bottom:8px}
+.card-fix .card-title {float:left;max-width:55%;margin-right:10%;text-overflow:ellipsis;overflow:hidden;white-space:nowrap;font-size:1.6rem;font-weight:600;color:#333}
+.card-fix .card-theard {float:left;max-width:30%;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;color:#676767}
+.card-fix .text-muted {font-size:1.4rem}
+.card-content, .card-footer {padding:0px}
+</style>
 <div class='panel-section'>
   <div class='panel-heading'>
-    <div class='title strong'><i class='icon icon-reply'></i> {$lang->user->reply}</div>
+    <div class='title strong'>{$lang->user->reply}</div>
   </div>
   <div class='cards condensed cards-list'>
     {foreach($replies as $reply)}
       <a href='{$control->createLink('thread', 'view', "id=$reply->thread") . "#$reply->id"}' class='card'>
-        <div class='card-heading'>
-          <h5>{$reply->title}</h5>
-        </div>
-        <div class='card-content text-muted'>
-          {$lang->reply->addedDate} {!substr($reply->addedDate, 2, -3)}
+        <div class='card-fix'>
+          <div class='card-top'>
+            <div class='card-title'>{$reply->title}</div>
+            <div class='card-theard'>{$reply->boardName}</div>
+          </div>
+          <div class='card-body'>{$reply->content}</div>
+          <div class='card-content text-muted'>
+            {$lang->reply->addedDate} {!substr($reply->addedDate, 2, -3)}
+          </div>
         </div>
       </a>
     {/foreach}
