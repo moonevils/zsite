b64a736436a159044cb6fcdc3446660f1fe5519b
wangyuting2 <851424971@qq.com>
2019-02-21 14:08:49 +0800

    * Adjust user edit realname bug.

--- system/template/mobile/user/editinfo.html.php
+++ system/template/mobile/user/editinfo.html.php
@@ -20,13 +20,40 @@
       <form id='editProfileForm' method='post' action="{!inlink('edit')}" data-checkfingerprint='1'>
         <div class='form-group form-pad-list'>
           <div class='form-group pad-label-left'>
-            {if(strpos('qq,phone,zipcode', $field) !== false)}
-            <input type='number' id='{$field}' name='{$field}' value='{$user->$field}' class='form-control'/>
+            {if($field == 'realname')}
+              {if($user->admin == 'super')}
+                {if(count(explode(',', $control->config->enabledLangs)) > 1)}
+                  <div class='input-group'>
+                    {if(strpos($control->config->enabledLangs, 'zh-cn') !== false)}
+                      <label class='input-group-addon'>{$config->langs['zh-cn']}</label>
+                      {!html::input("realnames[cn]", isset($user->realnames->cn) ? $user->realnames->cn : '', "class='form-control'")}
+                    {/if}
+                    {if(strpos($control->config->enabledLangs, 'zh-tw') !== false)}
+                      <label class='input-group-addon'>{!echo $config->langs['zh-tw']}</label>
+                      {!html::input("realnames[tw]", isset($user->realnames->tw) ? $user->realnames->tw : '', "class='form-control'")}
+                    {/if}
+                    {if(strpos($control->config->enabledLangs, 'en') !== false)}
+                      <label class='input-group-addon'>{!echo $config->langs['en']}</label>
+                      {!html::input("realnames[en]", isset($user->realnames->en) ? $user->realnames->en : '', "class='form-control'")}
+                    {/if}
+                  </div>
+                {else}
+                  {$clientLang = $control->config->defaultLang}
+                  {$clientLang = strpos($clientLang, 'zh-') !== false ? str_replace('zh-', '', $clientLang) : $clientLang}
+                  {!html::input("realnames[{{$clientLang}}]", $user->realname, "class='form-control'")}
+                {/if}
+              {else}
+                {!html::input($field, $user->$field, "class='form-control'")}
+                <label for='{$field}'>{$lang->user->$field}</label>
+              {/if}
+            {elseif(strpos('qq,phone,zipcode', $field) !== false)}
+              <input type='number' id='{$field}' name='{$field}' value='{$user->$field}' class='form-control'/>
+              <label for='{$field}'>{$lang->user->$field}</label>
             {else}
-            {!html::input($field, $user->$field, "class='form-control'")}
+              {!html::input($field, $user->$field, "class='form-control'")}
+              <label for='{$field}'>{$lang->user->$field}</label>
             {/if}
             {!html::input('field', $field, "class='hide'")}
-            <label for='{$field}'>{$lang->user->$field}</label>
           </div>
         </div>
         <div class='form-group'>
